ee5bf65c1d69b3d9d446964fd0dbdf022ed48073
jrupinski <rupinskijakub@gmail.com>
2019-06-13 09:33:47 +0200

    Update .gitignore to skip byebug history

--- recap_exercise_1/lib/problems.rb
+++ recap_exercise_1/lib/problems.rb
@@ -53,7 +53,7 @@ end
 # find_bigrams("the theater is empty", ["cy", "em", "ty", "ea", "oo"])  # => ["em", "ty", "ea"]
 # find_bigrams("to the moon and back", ["ck", "oo", "ha", "at"])        # => ["ck", "oo"]
 def find_bigrams(str, bigrams)
-    string = str.split(" ").join("") # remove spaces from string
+    string = str.chomp(" ")
     string_bigrams = []
 
     # create every bigram and add it if it's in bigrams arg
