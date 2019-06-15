22a7fda1a386924165fb275d219d881643564a9d
jrupinski <rupinskijakub@gmail.com>
2019-06-15 20:52:36 +0200

    W1D1: Add passing thru every ele in Array#my_each

--- Ruby/W1D1/lib/enumerables.rb
+++ Ruby/W1D1/lib/enumerables.rb
@@ -2,6 +2,12 @@ class Array
   # call the block on every element of array, and returns original array
   # DO NOT USE enumerables #each method
   def my_each(&block)
-    # TODO
+    array_length = self.length
+    for i in 0...array_length
+      puts "#{self[i]}"
+    end
   end
 end
+
+# tests
+[1, 2, 6, 3].my_each
