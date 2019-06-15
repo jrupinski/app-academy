6b4aad44c5cb8a97f3870dc00689d5bef1ab7475
jrupinski <rupinskijakub@gmail.com>
2019-06-15 18:54:26 +0200

    Make method a bit more descriptive

--- stuff/renaming.rb
+++ stuff/renaming.rb
@@ -5,8 +5,9 @@ end
 
 # more descriptive version
 def child_retirement_budget(your_age)
-    child_age = (your_age / 2)
-    years_until_retirement = 65 - child_age
+    child_age = your_age / 2
+    retirement_age = 65
+    years_until_retirement =retirement_age - child_age
     childs_current_budget = child_age ** 2
 
     childs_current_budget * years_until_retirement
