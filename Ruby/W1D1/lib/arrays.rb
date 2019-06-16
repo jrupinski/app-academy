e521c110a7368a77b562810f555b5b9e45ea571b
jrupinski <rupinskijakub@gmail.com>
2019-06-16 21:10:14 +0200

    arrays.rb: implement Array#my_flatten

--- Ruby/W1D1/lib/arrays.rb
+++ Ruby/W1D1/lib/arrays.rb
@@ -1,6 +1,22 @@
-class Array
-  def my_flatten
+require "byebug"
+require_relative "./enumerables.rb"
 
+class Array
+  # Turn multi-dimensional array into a flat 1-D array
+  def my_flatten
+    # base case
+    return self if self.is_a?(Array) == false
+    flattened = []
+
+    self.my_each do |ele|
+      if !ele.is_a?(Array)
+        flattened += [ele]
+      else
+        flattened += ele.my_flatten
+      end
+    end
+
+    flattened
   end
 
   def my_zip
@@ -19,3 +35,10 @@ class Array
 
   end
 end
+
+# Tests
+test = [1, [2], [[3]], [4], [[[[[5]]]]]]
+
+puts "#my_flatten test"
+p test.my_flatten # => [1, 2, 3, 4, 5]
+p ["test", "testing"].my_flatten # => ["test", "testing"]
