51a639b13845048619333d60955d9daf1dedf2c7
jrupinski <rupinskijakub@gmail.com>
2019-06-17 09:19:55 +0200

    arrays.rb: Fix inserting #my_zip array values from self arg

--- Ruby/W1D1/lib/arrays.rb
+++ Ruby/W1D1/lib/arrays.rb
@@ -26,8 +26,16 @@ class Array
     outer_arr_len = self.length
     ele_length = arg.length + 1
 
+    # initialize empty array with proper sizing
     new_array = Array.new(outer_arr_len) { Array.new(ele_length) }
     
+    # get elements from self
+    (0...outer_arr_len).each { |i| new_array[i][0] = self[i] }
+
+    # get elements from arguments
+    # TODO: FIX THIS
+    # (0...outer_arr_len).each { |i| new_array[i] << arg.my_flatten[i] }
+
     new_array
   end
 
@@ -50,3 +58,14 @@ test = [1, [2], [[3]], [4], [[[[[5]]]]]]
 puts "#my_flatten test"
 p test.my_flatten # => [1, 2, 3, 4, 5]
 p ["test", "testing"].my_flatten # => ["test", "testing"]
+
+puts "#my_zip test"
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
