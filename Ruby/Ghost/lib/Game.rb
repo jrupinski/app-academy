6dbee9373ba0a80c20544ea3ee3f504e27ace83c
jrupinski <rupinskijakub@gmail.com>
2019-07-02 20:42:37 +0200

    Ghost: Add dictionary file, empty methods

--- Ruby/Ghost/lib/Game.rb
+++ Ruby/Ghost/lib/Game.rb
@@ -1,5 +1,27 @@
 def Game
   def initialize
+    @dictionary = Set.new
+    @players = Hash.new
     @fragment = ""
+  end
+
+  def self.current_player
+    # TODO 
+  end
+
+  def self.previous_player
+    # TODO
+  end
+  
+  def self.next_player!
+    # TODO
+  end
+
+  def self.take_turn(player)
+    # TODO
+  end
+
+  def self.valid_play?(string)
+    # TODO
   end
 end
