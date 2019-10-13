fc6f0f52a064b2c00587400a55e6e09bcf89b2cd
jrupinski <rupinskijakub@gmail.com>
2019-10-14 00:05:19 +0200

    Add finished monkey patching exercise recap

--- recap/monkey_patching_project/lib/array.rb
+++ recap/monkey_patching_project/lib/array.rb
@@ -32,4 +32,43 @@ class Array
 
     ele_count
   end
+
+  # Part 2
+  def my_count(value)
+    count = 0
+
+    self.each { |ele| count +=1 if ele.eql?(value) }
+
+    count
+  end
+
+  def my_index(value)
+    return nil if self.empty? || !self.include?(value)
+
+    self.each.with_index { |ele, idx| return idx if ele.eql?(value) }
+  end
+
+  def my_uniq
+    no_dupl = []
+
+    self.each { |ele| no_dupl << ele if !no_dupl.include?(ele) }
+
+    no_dupl
+  end
+
+  def my_transpose
+    result = []
+
+    self.each_with_index do |row, row_idx|
+      
+      trans_row = []
+      row.each_with_index do |col, col_idx|
+        trans_row << self[col_idx][row_idx]
+      end
+
+      result << trans_row
+    end
+
+    result
+  end
 end
