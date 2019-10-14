fe3c68f87f2a47d8a1d3d0edb3aa91b6ae0405a6
jrupinski <rupinskijakub@gmail.com>
2019-10-14 21:49:09 +0200

    Add finished guessing_game recap; TODO: fix #rand test

---
+++ recap/guessing_game_project/lib/guessing_game.rb
@@ -0,0 +1,37 @@
+class GuessingGame
+  def initialize(min, max)
+    @min = min
+    @max = max
+    @secret_num = Random.new.rand(min..max)
+    @num_attempts = 0
+    @game_over = false
+  end
+
+  attr_accessor :num_attempts
+
+  def game_over?
+    @game_over
+  end
+
+  def check_num(num)
+    if num == @secret_num
+      @game_over = true
+      p "you win"
+      return self.game_over?
+
+    elsif num > @secret_num
+      p "too big"
+
+    else
+      p "too small"
+    end
+  
+  @num_attempts += 1    
+  end
+
+  def ask_user
+    p "enter a number: "
+    num = gets.chomp.to_i
+    self.check_num(num)
+  end
+end
