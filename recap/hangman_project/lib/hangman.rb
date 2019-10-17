c0cd9da0c4ba37a6a72fff52ca8e58ec515ae1c1
jrupinski <rupinskijakub@gmail.com>
2019-10-17 19:40:21 +0200

    Finish hangman project recap

--- recap/hangman_project/lib/hangman.rb
+++ recap/hangman_project/lib/hangman.rb
@@ -1,4 +1,7 @@
+require "byebug"
+
 class Hangman
+  # PART 1
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
   def self.random_word
@@ -28,5 +31,56 @@ class Hangman
     indices.each { |i| @guess_word[i] = char }
   end
 
-  
+  # PART 2
+  def try_guess(char)
+    if self.already_attempted?(char)
+      puts "that was already attempted"
+      return false
+    end
+
+    attempted_chars << char
+
+    if @secret_word.include?(char)
+      indices = self.get_matching_indices(char)
+      self.fill_indices(char, indices)
+    else
+      self.remaining_incorrect_guesses -= 1
+    end
+
+    true
+  end
+
+  def ask_user_for_guess
+    print "Enter a char: "
+    guess = gets.chomp.to_s
+    self.try_guess(guess)
+  end
+
+  def win?
+    # debugger
+    if @guess_word.join("") == @secret_word
+      puts "YOU WIN!"
+      return true
+    end
+    
+    false
+  end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      puts "YOU LOSE"
+      return true
+    end
+
+    false
+  end
+
+  def game_over?
+    if self.lose? || self.win?
+      puts "Secret word: #{@secret_word}"
+      true
+    else
+      false
+    end
+  end
 end
