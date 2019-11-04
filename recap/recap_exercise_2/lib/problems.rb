0bf5feda9ba91db5a1283673d394b0760ba0485a
jrupinski <rupinskijakub@gmail.com>
2019-11-04 10:34:36 +0100

    Add recap_exercise_3, create rspec for #no_dupes?

--- recap/recap_exercise_2/lib/problems.rb
+++ recap/recap_exercise_2/lib/problems.rb
@@ -72,7 +72,6 @@ class Array
           sorted = false
         end
       end
-
     end
 
     self
---
+++ recap/recap_exercise_3/spec/problems_spec.rb
@@ -0,0 +1,11 @@
+require "problems"
+
+describe "Recap Exercise 3" do
+  describe "no_dupes?" do
+    it "accepts an array as an arg" do
+      no_dupes([1,3,6,3,8])
+    end
+
+  end
+
+end
