a5c97336be25b3da037410aad1c3e809b6447938
jrupinski <rupinskijakub@gmail.com>
2019-11-04 11:30:20 +0100

    Implement #no_consecutive_repeats?

--- recap/recap_exercise_3/lib/problems.rb
+++ recap/recap_exercise_3/lib/problems.rb
@@ -6,3 +6,10 @@ def no_dupes?(arr)
     arr.count(ele) > 1
   end
 end
+
+def no_consecutive_repeats?(arr)
+  (0...arr.length - 1).each do |i|
+    return false if arr[i] == arr[i + 1]
+  end
+  true
+end
