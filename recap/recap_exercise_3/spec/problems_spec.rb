f768f75f93c2c2786aa6ee6befd3e31cd23b367b
jrupinski <rupinskijakub@gmail.com>
2019-11-04 11:27:38 +0100

    Create #no_consecutive_repeats(arr) rspec tests

--- recap/recap_exercise_3/spec/problems_spec.rb
+++ recap/recap_exercise_3/spec/problems_spec.rb
@@ -1,8 +1,10 @@
 require "problems"
 
 describe "Recap Exercise 3" do
-  let(:dupes_arr) { [1,3,6,3,8] }
+  
   describe "no_dupes?" do
+    dupes_arr = [1,3,6,3,8]
+
     it "accepts an array as an arg" do
       expect { no_dupes?(dupes_arr) }.to_not raise_error
     end
@@ -15,6 +17,24 @@ describe "Recap Exercise 3" do
     
     it "does not modify original array" do
       expect(dupes_arr).to eq([1,3,6,3,8])
+    end
+  end
+
+  describe "no_consecutive_repeats?" do
+    it "accepts an array as an arg" do
+      expect { no_consecutive_repeats?([10, 2]) }.to_not raise_error
+    end
+
+    it "returns true if an element never appears consecutively in the array" do
+      expect(no_consecutive_repeats?(['cat', 'dog', 'mouse', 'dog'])).to eq(true)
+      expect(no_consecutive_repeats?(['x'])).to eq(true)
+      expect(no_consecutive_repeats?([])).to eq(true)
+    end
+
+    it "returns false if same element appears consecutively in the array" do
+      expect(no_consecutive_repeats?(['cat', 'dog', 'dog', 'mouse'])).to eq(false)
+      expect(no_consecutive_repeats?([55, 55, 11, 7])).to eq(false)
+      expect(no_consecutive_repeats?(["x", 53, "y", "y", 99])).to eq(false)
     end
   end
 
