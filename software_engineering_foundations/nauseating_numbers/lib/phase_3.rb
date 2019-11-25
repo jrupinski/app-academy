8b18e40f64e5e44b8c6ad7b319c155fd5538d81a
jrupinski <rupinskijakub@gmail.com>
2019-11-25 23:14:28 +0100

    Refactor and fix #same_array_size? helper method

--- software_engineering_foundations/nauseating_numbers/lib/phase_3.rb
+++ software_engineering_foundations/nauseating_numbers/lib/phase_3.rb
@@ -2,8 +2,7 @@ require "byebug"
 require_relative "./phase_2.rb"
 
 def matrix_addition_reloaded(*matrices)
-  # debugger
-  return nil if !same_array_size?(matrices)
+  return nil if !same_array_size?(*matrices)
 
   mat_sum = matrices[0]
   matrices[1...matrices.length].each do |curr_matrix|
@@ -12,45 +11,20 @@ def matrix_addition_reloaded(*matrices)
 
   mat_sum
 end
-  # return matrix_addition(arrays[0], arrays[1]) if arrays.length == 2
 
-  # matrix_addition(arrays[0], matrix_addition_reloaded(arrays[1...arrays.length]))
-  # mat_sum = []
+def same_array_size?(*matrices)
+  (0...matrices.length - 1).each do |mat_idx|
+    curr_matrix = matrices[mat_idx]
+    next_matrix = matrices[mat_idx + 1]
 
-  # (0...arrays.length - 1).each do |arr_num|
-  #   mat_1 = arrays[arr_num]
-  #   mat_2 = arrays[arr_num + 1]
+    # compare num of rows in every matrix
+    return false if curr_matrix.length != next_matrix.length
 
-  #   (0...mat_1.length).each do |row|
-  #     curr_row = []
-
-  #     (0...mat_1[row].length).each do |col|
-  #       curr_row = mat_1[row][col] + mat_2[row][col]
-  #     end
-
-  #     mat_sum << curr_row
-  #   end
-  # end
-
-  # mat_sum
-  # end
-
-def same_array_size?(*arrays)
-  arrays.all? do |array|
-    (0...array.length - 1).all? do |sub_arr|
-      array[sub_arr].length == array[sub_arr + 1].length
+    # compare num of elements of every row in every matrix 
+    (0...curr_matrix.length).each do |row|
+        return false if curr_matrix[row].length != next_matrix[row].length
     end
   end
+
+  true
 end
-
-# def matrix_addition(mat_1, mat_2)
-#   mat_sum = Array.new(mat_1.length) { Array.new(mat_1[0].length) }
-
-#   (0...mat_1.length).each do |row|
-#     (0...mat_1[row].length).each do |col|
-#       mat_sum[row][col] = mat_1[row][col] + mat_2[row][col]
-#     end
-#   end
-
-#   mat_sum
-# end
