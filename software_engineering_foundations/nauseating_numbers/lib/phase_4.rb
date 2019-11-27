3fb5669d800e5fc5d32d652d31db5dea9c0dc9a5
jrupinski <rupinskijakub@gmail.com>
2019-11-27 19:38:02 +0100

    Implement #triangular_word? based on test suite
    Why:
    - Implement helper method #is_triangular?

--- software_engineering_foundations/nauseating_numbers/lib/phase_4.rb
+++ software_engineering_foundations/nauseating_numbers/lib/phase_4.rb
@@ -50,3 +50,50 @@ def is_mersenne?(prime)
 
   2.pow(i) - 1 == prime
 end
+
+#
+# Check if word, when encoded using sum of it's letters in the alphabet,
+# is triangular. 
+# Example:
+# "cat" encoded is 24 because "c" is the 3rd of the alphabet, "a" is the 1st, 
+# and "t" is the 20th:
+# 3 + 1 + 20 = 24
+#
+# @param [String] word Lowercase word
+#
+# @return [Boolean] Indicates if sum of encoded chars in word is triangular
+#
+def triangular_word?(word)
+  # debugger
+  alpha = ("a".."z").to_a
+
+  word_encoded = word.chars.inject(0) do |sum, char|
+    char_encoded = alpha.index(char) + 1 # start alpha encoding at 1, not 0
+    sum += char_encoded
+  end
+
+  is_triangular?(word_encoded)
+end
+
+#
+# Checks if given number is triangular.
+# A triangular number is a number of the form (i * (i + 1)) / 2 where i is
+# some positive integer.
+# Substituting i with increasing integers gives the triangular number sequence.
+#
+# @param [Integer] num A positive number
+#
+# @return [Boolean] Boolean indicating if number is triangular
+#
+def is_triangular?(num)
+  tri_nums = [1]
+
+  i = 2
+  until tri_nums.last >= num
+    tri_number = (i * (i + 1)) / 2
+    tri_nums << tri_number
+    i += 1
+  end
+
+  tri_nums.last == num
+end
