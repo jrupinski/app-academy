176aee3bf6b20c4d651c19f3f2bc173a1c649960
jrupinski <rupinskijakub@gmail.com>
2019-11-27 18:15:48 +0100

    Add doc for #mersenne_prime, fix #is_prime? bug
    Why:
    - Add documentation comments for #mersenne_prime and helper methods
    - Fix wrong argument type bug in #is_prime?

--- software_engineering_foundations/nauseating_numbers/lib/phase_4.rb
+++ software_engineering_foundations/nauseating_numbers/lib/phase_4.rb
@@ -1,18 +1,14 @@
 require "byebug"
 
 #
-# Get n-th mersenne prime 
+# Get n-th Mersenne prime 
 # 
-# A "Mersenne prime" is a prime number that is one less than a power of 2.
-# This means that it is a prime number with the form 2^x - 1, where x is some exponent. 
+# @param [Integer] n Positive number
 #
-# @param [Integer] n Positive-only number
-#
-# @return [Integer] n-th mersenne prime number
+# @return [Integer] n-th Mersenne prime number
 #
 def mersenne_prime(n)
   raise "arg has to be a positive num" if !n.is_a?(Integer) || n < 1
-  # debugger
   mer_primes = [] 
   i = 1
   until mer_primes.length == n
@@ -23,16 +19,34 @@ def mersenne_prime(n)
   mer_primes.last
 end
 
+#
+# Check if number is a Prime number
+#
+# @param [Integer] num Number to check
+#
+# @return [Boolean] Return true if number is prime, false otherwise
+#
 def is_prime?(num)
-  return false if num < 2
+  return false if num < 2 || !num.is_a?(Integer)
   (2...num).none? { |divisor| num % divisor == 0 }
 end
 
-def is_mersenne?(num)
+#
+# Check if prime is a Mersenne prime
+#
+# A "Mersenne prime" is a prime number that is one less than a power of 2.
+# This means that it is a prime number with the form 2^x - 1, where x is 
+# some exponent. 
+#
+# @param [Integer] prime Prime number.
+#
+# @return [Boolean] True if prime is a Mersenne prime, false otherwise.
+#
+def is_mersenne?(prime)
   i = 1
-  while 2.pow(i) < num
+  while 2.pow(i) < prime
     i += 1
   end
 
-  2.pow(i) - 1 == num
+  2.pow(i) - 1 == prime
 end
