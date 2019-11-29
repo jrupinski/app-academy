86762471506e25fb042b11df8df39f511c24d647
jrupinski <rupinskijakub@gmail.com>
2019-11-29 03:03:56 +0100

    Implement #pretentious_primes based on test suite
    Why:
    - Create helper methods: #nearest_prime, #is_prime?, #get_n_primes,
      #get_primes
    - Document every method in class/files for easy YARD doc generation

--- software_engineering_foundations/nauseating_numbers/lib/phase_4.rb
+++ software_engineering_foundations/nauseating_numbers/lib/phase_4.rb
@@ -153,3 +153,128 @@ end
 def is_consecutive?(num_1, num_2)
   num_1 + 1 == num_2 || num_1 - 1 == num_2
 end
+
+#
+# The method should return a new array where each element of the original array 
+# is replaced according to the following rules:
+#
+#   - when the number argument is positive, replace an element with the n-th 
+#   nearest prime number that is greater than the element
+# 
+#   - when the number argument is negative, replace an element with the n-th
+#   nearest prime number that is less than the element
+# 
+# Note that we will always be able to find a prime that is greater than a given 
+# number because there are an infinite number of primes (this is given by 
+# Euclid's Theorem). However, we may be unable to find a prime that is smaller 
+# than a given number, because 2 is the smallest prime. When a smaller prime 
+# cannot be calculated, replace the element with nil.
+#
+# @param [Array] arr Array of whole numbers
+# @param [Integer] num Whole number
+#
+# @return [Array] New array with replaced elements (if needed)
+#
+def pretentious_primes(arr = nil, n = nil)
+  # error checking
+  if arr.nil? || n.nil? || !arr.is_a?(Array) || !n.is_a?(Integer) || 
+      arr.any? { |ele| !ele.is_a?(Integer) }
+    raise "Use array of numbers and a number as arguments"
+  end
+  
+  # debugger
+  return arr if n == 0
+  arr.map { |num| nearest_prime(num, n) }
+end
+
+#
+# Get n-th next/previous prime relative to given number. Return biggest 
+# prime if n == 0. If smaller prime neighbor is less than 2 - replace 
+# it with nil.
+#
+# Example:
+# nearest_prime(5, 0)   => 5
+# nearest_prime(5, 1)   => 7
+# nearest_prime(5, -1)  => 3
+# nearest_prime(5, -2)  => 2
+# nearest_prime(5, -3)  => nil
+#
+#
+# @param [Integer] num A number to look for prime for
+# @param [Integer] n A positive or negative number, indicating n-th prime
+#
+# @return [Integer || nil] n-th previous/next prime number. Return nil if 
+# n-th previous prime is less than 2.
+#
+def nearest_prime(num, n)
+  primes = get_primes(num)
+  max_prime_idx = primes.length - 1
+
+  # debugger
+  if n < 0
+    smaller_prime_idx = max_prime_idx + n
+    # if num is bigger than it's max prime - count going to it as one of N steps
+    smaller_prime_idx += 1 if num > primes.last
+
+    if smaller_prime_idx < 0
+      nil
+    else
+      primes[smaller_prime_idx]
+    end
+
+  else
+    get_n_primes(primes.length + n).last
+  end
+end
+
+#
+# Check if a number is prime
+#
+# @param [Integer] num Number to check if it's prime
+#
+# @return [Boolean] Indication if number is a prime number
+#
+def is_prime?(num)
+  return false if num < 2
+  (2...num).none? { |div| num % div == 0 }
+end
+
+#
+# Get all primes of given number.
+#
+# @param [Integer] num A number to get primes off of.
+#
+# @return [Array] An array of primes up to number argument.
+#
+def get_primes(num)
+  return nil if num < 2
+  
+  primes = []
+  i =  2
+  until i > num
+    primes << i if is_prime?(i)
+    i += 1
+  end
+
+  primes
+end
+
+#
+# Create an array filled with n consecutive primes.
+#
+# @param [Integer] n How many primes to include
+#
+# @return [Array] An array filled with n-amount primes.
+#
+def get_n_primes(n)
+  return [] if n < 1
+  primes = []
+
+  i = 2
+  until primes.length == n
+    primes << i if is_prime?(i)
+    i += 1
+  end
+
+  primes
+end
