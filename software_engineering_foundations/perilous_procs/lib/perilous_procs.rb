87c9c244593d4b02bbe8c8a25d87f3c0dbc0db10
jrupinski <rupinskijakub@gmail.com>
2019-11-30 12:21:28 +0100

    Implement ::exactly?

--- software_engineering_foundations/perilous_procs/lib/perilous_procs.rb
+++ software_engineering_foundations/perilous_procs/lib/perilous_procs.rb
@@ -3,3 +3,9 @@ def some?(arr, &block)
   arr.each { |ele| return true if block.call(ele) }
   false
 end
+
+def exactly?(arr, n, &block)
+  count = 0
+  arr.each { |ele| count += 1 if block.call(ele) }
+  count == n
+end
