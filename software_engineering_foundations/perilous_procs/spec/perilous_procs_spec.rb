7529202b090d2158f961d9e597f3963f819caeef
jrupinski <rupinskijakub@gmail.com>
2019-11-30 12:07:07 +0100

    Fix test spec indentation, fix raise_error typo

--- software_engineering_foundations/perilous_procs/spec/perilous_procs_spec.rb
+++ software_engineering_foundations/perilous_procs/spec/perilous_procs_spec.rb
@@ -1,25 +1,25 @@
 require "perilous_procs"
 
 describe "phase 1" do
-    describe "some?" do
-        it "accepts an array and a block as arguments" do
-            expect { some?([3, 1, 11, 5]) { |n| n.even? } }.to_not raise error
-        end
-
-        it "return a boolean indicating if any element is true when given to block" do
-            expect(some?([3, 1, 11, 5]) { |n| n.even? }).to eq(false)
-            expect(some?([3, 4, 11, 5]) { |n| n.even? }).to eq(true)
-            expect(some?([8, 2]) { |n| n.even? }).to eq(true)
-            expect(some?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }).to eq(false)
-            expect(some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' }).to eq(true)
-            expect(some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' }).to eq(true)
-        end
-
-        it "uses Array#each" do
-            arr = [3, 1, 11, 5]
-            # block = Proc.new { |n| n.even? }
-            expect(arr).to receive(:each).at_least(:once)
-            some?(arr) { |n| n.even? }
-        end
+  describe "some?" do
+    it "accepts an array and a block as arguments" do
+      expect { some?([3, 1, 11, 5]) { |n| n.even? } }.to_not raise_error
     end
+
+    it "return a boolean indicating if any element is true when given to block" do
+      expect(some?([3, 1, 11, 5]) { |n| n.even? }).to eq(false)
+      expect(some?([3, 4, 11, 5]) { |n| n.even? }).to eq(true)
+      expect(some?([8, 2]) { |n| n.even? }).to eq(true)
+      expect(some?(['squash', 'corn', 'kale', 'carrot']) { |str| str[0] == 'p' }).to eq(false)
+      expect(some?(['squash', 'corn', 'kale', 'potato']) { |str| str[0] == 'p' }).to eq(true)
+      expect(some?(['parsnip', 'lettuce', 'pea']) { |str| str[0] == 'p' }).to eq(true)
+    end
+
+    it "uses Array#each" do
+      arr = [3, 1, 11, 5]
+      # block = Proc.new { |n| n.even? }
+      expect(arr).to receive(:each).at_least(:once)
+      some?(arr) { |n| n.even? }
+    end
+  end
 end
