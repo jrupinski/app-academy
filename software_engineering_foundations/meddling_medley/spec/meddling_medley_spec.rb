3f4a7c09044b56c2bf4bc9ed5d075f546f5710ea
jrupinski <rupinskijakub@gmail.com>
2019-12-05 03:42:51 +0100

    Add ::disjunct_select test suite

--- software_engineering_foundations/meddling_medley/spec/meddling_medley_spec.rb
+++ software_engineering_foundations/meddling_medley/spec/meddling_medley_spec.rb
@@ -198,4 +198,31 @@ context "Phase 2: More difficult, maybe?" do
       end
     end
   end
+
+  describe "disjunct_select" do
+    let (:longer_four) { Proc.new { |s| s.length > 4 } }
+    let (:contains_o) { Proc.new { |s| s.include?('o') } }
+    let (:starts_a) { Proc.new { |s| s[0] == 'a' } }
+
+    it "accepts an array and one or more procs as arguments" do
+      expect { disjunct_select(["dog", "yass"], longer_four) }.to_not raise_error
+    end
+
+    it "return a new array containing the elements that return true when passed into at least one of the given procs" do
+      expect(disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+          longer_four,
+      )).to eq(["apple", "teeming"])
+
+      expect(disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+          longer_four,
+          contains_o
+      )).to eq(["dog", "apple", "teeming", "boot"])
+
+      expect(disjunct_select(['ace', 'dog', 'apple', 'teeming', 'boot', 'zip'],
+          longer_four,
+          contains_o,
+          starts_a
+      )).to eq(["ace", "dog", "apple", "teeming", "boot"])
+    end
+  end
 end
