5a12cc1fc5ec11496f0438a03158f7422e402740
jrupinski <rupinskijakub@gmail.com>
2019-12-15 22:49:27 +0100

    Implement List class

--- software_engineering_foundations/todo_board_project/lib/list.rb
+++ software_engineering_foundations/todo_board_project/lib/list.rb
@@ -1,5 +1,47 @@
+require_relative "./item"
+
 class List
-  def initialize
-    # TODO
+  attr_accessor :label
+
+  def initialize(label)
+    @label = label
+    @items = []
+  end
+
+  def add_item(title, deadline, description="")
+    begin
+      new_item = Item.new(title, deadline, description)
+    rescue => exception
+      puts exception.message
+      false
+    else
+      @items << new_item
+      true
+    end
+  end
+
+  def size
+    @items.count
+  end
+
+  def valid_index?(index)
+    (0...@items.count).include?(index)
+  end
+
+  def swap(index_1, index_2)
+    if valid_index?(index_1) && valid_index?(index_2)
+    @items[index_1], @items[index_2] = @items[index_2], @items[index_1]
+      true
+    else
+      false
+    end
+  end
+
+  def [](index)
+    valid_index?(index) ? @items[index] : nil
+  end
+
+  def priority
+    @items.first
   end
 end
