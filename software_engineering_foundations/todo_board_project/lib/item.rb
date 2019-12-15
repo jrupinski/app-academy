4961ffbc9a5cadad86317d39e11fe608366d277b
jrupinski <rupinskijakub@gmail.com>
2019-12-15 21:40:19 +0100

    Add Board::initialize, refactor Item::valid_date?

--- software_engineering_foundations/todo_board_project/lib/item.rb
+++ software_engineering_foundations/todo_board_project/lib/item.rb
@@ -1,34 +1,28 @@
 class Item
-  class InvalidDateError < StandardError; end
+  class InvalidDateError < StandardError
+    def message
+      "Invalid date. Correct format: YYYY-MM-DD"
+    end
+  end
 
-  def initialize
-    # TODO
+  def initialize(title, deadline, description)
+    raise InvalidDateError if !Item.valid_date?(deadline)
+    @title = title
+    @deadline = deadline
+    @description = description
   end
 
   def self.valid_date?(date)
     date_separated = date.split("-")
-    if date_separated.count != 3
-      puts "wrong date input"
-      return false 
-    end
+    # if wrong date input (not enough/too many date elements)
+    return false if date_separated.count != 3
 
     year = date_separated.first.to_i
     month = date_separated[1].to_i
     day = date_separated.last.to_i
-
-    if year < 0
-      # raise InvalidDateError
-      puts "invalid year" 
-    elsif month < 1 || month > 12
-      # raise InvalidDateError
-      puts "invalid month"
-      return false
-    elsif day < 1 || day > 31
-      # raise InvalidDateError
-      puts "invalid day"
-      return false
-    end
-
+    # if invalid date format
+    return false if year < 0 || month < 1 || month > 12 || day < 1 || day > 31
+    
     true
   end
 end
