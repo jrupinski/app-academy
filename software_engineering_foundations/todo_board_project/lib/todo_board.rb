c8f7f2a653ca69e04a7368673eeb1aa8478013e5
jrupinski <rupinskijakub@gmail.com>
2019-12-17 23:20:08 +0100

    Add TodoBoard#get_command

--- software_engineering_foundations/todo_board_project/lib/todo_board.rb
+++ software_engineering_foundations/todo_board_project/lib/todo_board.rb
@@ -1,8 +1,27 @@
+require_relative "./list"
+
 #
 # Todo Board, which contains a single list of items
 #
 class Todo_Board
+
+  class InvalidCommandError < StandardError; end
+
   def initialize(label)
     @list = List.new(label)
+  end
+
+  def get_command
+    valid_commands = ["mktodo", "up", "down", "swap", "sort", "priority", "print", "quit"]
+    puts "Available commands: mktodo, up, down, swap, sort, priority, print, quit"
+    print "Enter a command: "
+    input = gets.chomp
+    raise InvalidCommandError if !valid_commands.include?(input)
+    return nil if input == "quit"
+    do_command(input)
+  end
+
+  def do_command(command)
+    # TODO
   end
 end
