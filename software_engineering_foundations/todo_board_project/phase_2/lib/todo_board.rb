0027274583ee596d5a014372287c73b525ce31e8
jrupinski <rupinskijakub@gmail.com>
2019-12-19 00:19:43 +0100

    Add newlines to make code more readable

--- software_engineering_foundations/todo_board_project/phase_2/lib/todo_board.rb
+++ software_engineering_foundations/todo_board_project/phase_2/lib/todo_board.rb
@@ -23,17 +23,21 @@ class TodoBoard
     when "help"
       puts
       puts File.read("./lib/help.txt")
+      
     when "mklist"
       raise "Wrong num of arguments" if args.count != 1
       label = args[0]
       raise "List #{label} already exists!" if @lists.has_key?(label.to_sym)
       @lists[label.to_sym] = List.new(label)
       puts "List #{label} created!"
+      
     when "ls"
       puts "Available lists:"
       puts @lists.keys
+      
     when "showall"
       @lists.values.each { |list| puts list.print }
+      
     when "mktodo"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
@@ -43,6 +47,7 @@ class TodoBoard
       description = args[3..-1].join(" ") || ""
       @lists[list_label.to_sym].add_item(title, deadline, description)
       puts "item added!"
+      
     when "toggle"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
@@ -50,6 +55,7 @@ class TodoBoard
       raise "invalid item index" if index.nil?
       @lists[list_label.to_sym].toggle_item(index.to_i)
       puts "Done status toggled!"
+      
     when "rm"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
@@ -57,11 +63,13 @@ class TodoBoard
       raise "Valid Item index required" if index.nil?
       @lists[list_label.to_sym].remove_item(index.to_i)
       puts "deleted item!"
+      
     when "purge"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
       @lists[list_label.to_sym].purge
       puts "list #{list_label} purged!"
+      
     when "up"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
@@ -70,6 +78,7 @@ class TodoBoard
       raise "Valid Item index required" if index.nil?
       @lists[list_label.to_sym].up(index.to_i, amount.to_i)
       puts "moved item up by #{amount}"
+      
     when "down"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
@@ -78,6 +87,7 @@ class TodoBoard
       amount = args[2] || 1
       @lists[list_label.to_sym].down(index.to_i, amount.to_i)
       puts "moved item down by #{amount}"
+      
     when "swap"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
@@ -86,20 +96,24 @@ class TodoBoard
       raise "Two item indexes required." if item_1.nil? || item_2.nil?
       @lists[list_label.to_sym].swap(item_1.to_i, item_2.to_i)
       puts "items swapped!"
+      
     when "sort"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
       @lists[list_label.to_sym].sort_by_date!
       puts "Items sorted!"
+      
     when "priority"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
       @lists[list_label.to_sym].print_priority
+      
     when "print"
       list_label = args[0] || nil
       raise "wrong list label" if list_label.nil? || !@lists.has_key?(list_label.to_sym)
       item_idx = args[0]
       item_idx.nil? ? @lists[list_label.to_sym].print : @lists[list_label.to_sym].print_full_item(item_idx.to_i)
+      
     when "quit"
       puts "Quitting..."
       return nil
