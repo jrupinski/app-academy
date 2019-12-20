8c09009ea6bce43e703ac1ced7df02ec41419e4f
jrupinski <rupinskijakub@gmail.com>
2019-12-20 16:09:35 +0100

    Implement Array#my_each

--- ruby/enumerables/enumerables.rb
+++ ruby/enumerables/enumerables.rb
@@ -1,4 +1,18 @@
 # Methods for Ruby->Enumerables exercise
-def my_each
-  # stuff
+class Array
+  #
+  # Extend the Array class to include a method named my_each that takes a block, calls the block on every element of the array, and returns the original array. Do not use Enumerable's each method.
+  #
+  # @param [Proc] &block Block to execute
+  #
+  # @return [Array] Original array
+  #
+  def my_each(&block)
+    self.size.times do |index|
+      ele = self[index]
+      block.call(ele)
+    end
+
+    self
+  end
 end
