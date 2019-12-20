d1a5ac14d167f0be903e42b505864e5eace73dc7
jrupinski <rupinskijakub@gmail.com>
2019-12-20 16:41:58 +0100

    Add Array#my_flatten

--- ruby/enumerables/arrays.rb
+++ ruby/enumerables/arrays.rb
@@ -1,1 +1,18 @@
 # Array methods for Ruby->Enumerables exercise
+class Array
+  #
+  # Return all elements of the array into a new, one-dimensional array. Hint: use recursion!
+  #
+  # @return [Array] 1-dimensional array with all elements of original array
+  #
+  def my_flatten
+    return [self] if !self.is_a?(Array)
+    
+    flattened = []
+    self.each do |ele|
+      ele.is_a?(Array) ? flattened += ele.my_flatten : flattened << ele 
+    end
+    
+    flattened
+  end
+end
