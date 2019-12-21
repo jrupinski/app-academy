ed139b6f93e3fd237158ffab39d99a668514bf29
jrupinski <rupinskijakub@gmail.com>
2019-12-21 18:12:54 +0100

    Add iteration exercise file

---
+++ ruby/enumerables/iteration_exercises.rb
@@ -0,0 +1,6 @@
+# Review the iteration exercises from the prepwork. Implement the following methods:
+  #factors(num)
+  #bubble_sort!(&prc)
+  #bubble_sort(&prc)
+  #substrings(string)
+  #subwords(word, dictionary)
