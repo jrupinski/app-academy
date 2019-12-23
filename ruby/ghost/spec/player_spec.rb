91c3044355e7e7cdef543879ffa4aad61f527a57
jrupinski <rupinskijakub@gmail.com>
2019-12-23 17:20:53 +0100

    Add more tests for Player

--- ruby/ghost/spec/player_spec.rb
+++ ruby/ghost/spec/player_spec.rb
@@ -2,11 +2,11 @@ require "player"
 
 describe Player do
   it "creates a player with given name" do
-    expect { Player.new("name") }.to_not raise_error
-    expect { Player.new }.to raise_error
-    expect { Player.new(123) }.to raise_error
+    expect { Player.new("name") }.to_not raise_error 
+    expect { Player.new }.to raise_error ArgumentError
+    expect { Player.new(123) }.to raise_error Player::InvalidNameError
 
-    expect(Player.new("name")).to eq("name")
-    expect(Player.new("")).to eq("name")
+    expect(Player.new("name").name).to eq("name")
+    expect(Player.new("").name).to eq("")
   end
 end
