e3a99188f4d3d80ebc14b14feeacab8c2d1f1841
jrupinski <rupinskijakub@gmail.com>
2019-12-23 16:51:56 +0100

    Add empty methods of Game class

--- ruby/ghost/lib/game.rb
+++ ruby/ghost/lib/game.rb
@@ -8,4 +8,28 @@ class Game
     @dictionary = Set.new(dictionary_file)
     @players = [player_1, player_2]
   end
+
+  def play_round
+    # TODO
+  end
+
+  def current_player
+    # TODO
+  end
+
+  def previous_player
+    # TODO
+  end
+
+  def next_player!
+    # TODO
+  end
+
+  def take_turn(player)
+    # TODO
+  end
+
+  def valid_play?
+    # TODO
+  end
 end
