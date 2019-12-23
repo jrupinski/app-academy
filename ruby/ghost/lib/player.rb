d95c6f4b82f23dedaa2019259e9adba373b84a2e
jrupinski <rupinskijakub@gmail.com>
2019-12-23 17:21:08 +0100

    Add error class for Invalid Name

--- ruby/ghost/lib/player.rb
+++ ruby/ghost/lib/player.rb
@@ -2,18 +2,18 @@
 class Player
   attr_reader :name
   
-  class InvalidName < StandardError; end
-    
-    def initialize(name)
-      raise InvalidName if !name.is_a?(String)
-      @name = name
-    end
-    
-    def guess
-      # todo
-    end
+  class InvalidNameError < ArgumentError; end
+  
+  def initialize(name)
+    raise InvalidNameError if !name.is_a?(String)
+    @name = name
+  end
+  
+  def guess
+    # todo
+  end
 
-    def Alert_invalid_guess 
-      # todo
-    end
+  def Alert_invalid_guess 
+    # todo
+  end
 end
