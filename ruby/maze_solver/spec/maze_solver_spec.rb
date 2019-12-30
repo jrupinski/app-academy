a4d66de8f7c773991e8b1d84f101cd3ec2abd3a0
jrupinski <rupinskijakub@gmail.com>
2019-12-30 19:30:34 +0100

    Add test suite for MazeSolver

--- ruby/maze_solver/spec/maze_solver_spec.rb
+++ ruby/maze_solver/spec/maze_solver_spec.rb
@@ -1,5 +1,16 @@
 require "maze_solver"
 
 describe MazeSolver do
-  # TODO
+  let(:maze_solver) { MazeSolver.new }
+  let(:maze_file) { "spec/maze1.txt" }
+  let(:solved_maze) { "spec/maze1_solution.txt" }
+
+  it "Creates a new Maze Solver object" do
+    expect { MazeSolver.new(maze_file) }.to_not raise_error
+  end
+
+  it "Returns a solved maze" do
+    maze_solver = MazeSolver.new(maze_file)
+    expect(maze_solver.solve).to eq(solved_maze)
+  end
 end
