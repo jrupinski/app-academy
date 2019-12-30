06d974c499377468ac3305c1aa073bedefdfa3e7
jrupinski <rupinskijakub@gmail.com>
2019-12-30 20:32:24 +0100

    Add empty methods, add start and end positions

--- ruby/maze_solver/lib/maze_solver.rb
+++ ruby/maze_solver/lib/maze_solver.rb
@@ -2,7 +2,38 @@ class MazeSolver
   def initialize(filename)
     @filename = filename
     @maze_array = File.foreach(filename).map { |line| line.chomp }
+    @start = nil
+    @end = nil
+    
+    @maze_array.each_with_index do |line, row|
+      line.chars.each_with_index do |ele, col|
+        @start = [row, col] if ele == "S"
+        @end = [row, col] if ele == "E"
+      end
+    end
   end
+end
+
+def solve
+  # TODO
+end
+
+private
+
+def go_up
+  # TODO
+end
+
+def go_down
+  # TODO
+end
+
+def go_left
+  # TODO
+end
+
+def go_right
+  # TODO
 end
 
 if $PROGRAM_NAME == __FILE__
