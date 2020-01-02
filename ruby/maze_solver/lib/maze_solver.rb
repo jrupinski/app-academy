bc57a2905f1b341dea15d5c0f910be390c5c61e0
jrupinski <rupinskijakub@gmail.com>
2020-01-02 02:05:51 +0100

    Add maze moving methods, place mark method

--- ruby/maze_solver/lib/maze_solver.rb
+++ ruby/maze_solver/lib/maze_solver.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class MazeSolver
   def initialize(filename)
     @filename = filename
@@ -11,29 +13,44 @@ class MazeSolver
         @end = [row, col] if ele == "E"
       end
     end
+    
+    @current_pos = @start
   end
-end
 
-def solve
+  def solve
   # TODO
 end
 
-private
+  private
 
-def go_up
-  # TODO
-end
+  attr_reader :current_pos, :maze_array
 
-def go_down
-  # TODO
-end
+  def go_up
+    current_pos[0] -= 1
+    place_mark
+  end
 
-def go_left
-  # TODO
-end
+  def go_down
+    current_pos[0] += 1
+    place_mark
+  end
 
-def go_right
-  # TODO
+  def go_left
+    current_pos[1] -= 1
+    place_mark
+  end
+
+  def go_right
+    current_pos[1] += 1
+    place_mark
+  end
+
+  def place_mark
+    row = current_pos.first
+    col = current_pos.last
+    maze_array[row][col] = "X"
+    puts maze_array
+  end
 end
 
 if $PROGRAM_NAME == __FILE__
