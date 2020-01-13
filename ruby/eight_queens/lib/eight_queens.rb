b27f030c956ac047922d6b32942c6ec5f8693cf2
jrupinski <rupinskijakub@gmail.com>
2020-01-13 20:27:48 +0100

    Add method for checking hor, ver and diag attacks

--- ruby/eight_queens/lib/eight_queens.rb
+++ ruby/eight_queens/lib/eight_queens.rb
@@ -1,5 +1,8 @@
 require "set"
 
+#
+# Find positions of eight queens on 8x8 grid where they don't attack each other
+#
 class EightQueens
   def initialize
     @board = Array.new(8) { Array.new(8, false) }
@@ -8,11 +11,28 @@ class EightQueens
   def find_position
     # TODO
   end
+  
+  def no_piece_diagonally?(row)
+    board.each(&:one?)
+  end
+
+  def no_piece_vertically?(column)
+    board.transpose.each(&:one?)
+  end
+
+  def no_piece_horizontally?
+    diagonal_left = []
+    (0...board.length).each do |row|
+      column = row
+      diagonal_left << board[row][column]
+      diagonal_right << board[row].reverse[column]
+    end
+  end
 
   # private
   attr_reader :board
 end
   
-# if $PROGRAM_NAME == __FILE__
-#   EightQueens.new.find_position
-# end
+if $PROGRAM_NAME == __FILE__
+  EightQueens.new.find_position
+end
