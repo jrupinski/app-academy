4cab47985ddf427340a6bc685290c5fa891a2281
jrupinski <rupinskijakub@gmail.com>
2020-01-14 19:46:23 +0100

    Refactor names of methods, remove unneeded getter

--- ruby/eight_queens/lib/eight_queens.rb
+++ ruby/eight_queens/lib/eight_queens.rb
@@ -7,29 +7,30 @@ class EightQueens
   def initialize
     @board = Array.new(8) { Array.new(8, false) }
   end
+
+  private 
   
   def queens_positions
     positions = []
-    board.each.with_index do |line, row|
+    @board.each.with_index do |line, row|
       line.each.with_index { |position, col| positions << [row, col] if position == true }
     end
     positions
   end
   
   def no_conflicts?
-    no_attacks_vertically? && no_attacks_horizontally? && no_attacks_diagonally?
+    no_conflicts_vertically? && no_conflicts_horizontally? && no_conflicts_diagonally?
   end
-
-  def no_attacks_horizontally?
-    board.none? { |row| row.count(true) > 1 }
+  
+  def no_conflicts_horizontally?
+    @board.none? { |row| row.count(true) > 1 }
   end
-
-  def no_attacks_vertically?
-    board.transpose.none? { |column| column.count(true) > 1 }
+  
+  def no_conflicts_vertically?
+    @board.transpose.none? { |column| column.count(true) > 1 }
   end
-
-  # TODO: Check EVERY diagonal
-  def no_attacks_diagonally?
+  
+  def no_conflicts_diagonally?
     queens_positions.each_slice(2).none? do |queen_1, queen_2|
       queen_1_row, queen_1_col = queen_1.first, queen_1.last
       queen_2_row, queen_2_col = queen_2.first, queen_2.last
@@ -38,11 +39,8 @@ class EightQueens
       delta_column == delta_row
     end
   end
-
-  # private
-  attr_reader :board
 end
-  
+
 if $PROGRAM_NAME == __FILE__
   # TODO
 end
