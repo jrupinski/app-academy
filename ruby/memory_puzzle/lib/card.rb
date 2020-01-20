b9031f10eb0aaf4f0f6b0a845612e1c8375e6e70
jrupinski <rupinskijakub@gmail.com>
2020-01-20 11:08:04 +0100

    Implement Card class
    Why:
    - Add ::new
    - Add #show_value
    - Add private methods #reveal, #hide, #==, #to_s

--- ruby/memory_puzzle/lib/card.rb
+++ ruby/memory_puzzle/lib/card.rb
@@ -1,5 +1,32 @@
 class Card
-  def initialize
-    # TODO
+  def initialize(value)
+    @value = value
+    @face_up = false
+  end
+
+  def show_value
+    value
+  end
+  
+  private
+
+  def value
+    @face_up ? @value : nil
+  end
+  
+  def hide
+    @face_up = false
+  end
+
+  def reveal
+    @face_up = true
+  end
+
+  def to_s
+    @value.to_s
+  end
+
+  def ==(other_card)
+    self.to_s == other_card.to_s
   end
 end
