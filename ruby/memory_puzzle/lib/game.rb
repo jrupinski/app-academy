8d7c15c0892669f8dc4758f91cced8b84219e4a8
jrupinski <rupinskijakub@gmail.com>
2020-01-23 12:41:50 +0100

    Add Game::new, Game#populate_board, Game#play

--- ruby/memory_puzzle/lib/game.rb
+++ ruby/memory_puzzle/lib/game.rb
@@ -1,7 +1,23 @@
 require_relative "board.rb"
 
 class Game
-  def initialize
-    # TODO
+  def initialize(size = 4)
+    @board = Board.new(size)
+    @previous_guess = nil
   end
+
+  def play
+    # todo
+  end
+
+  private
+
+  def generate_board
+    @board.populate
+  end
+end
+
+
+if __FILE__ == $PROGRAM_NAME
+  Game.new.play
 end
