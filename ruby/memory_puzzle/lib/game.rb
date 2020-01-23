ed34f9c6c15f141d10c4b7abca7b731a8959398b
jrupinski <rupinskijakub@gmail.com>
2020-01-23 13:24:09 +0100

    Add methods for revealing cards and rendering
    Why:
    
    - Add #get_user_coordinates
    - Add #reveal_card
    - Add #render_board
    - Add base of Game#play

--- ruby/memory_puzzle/lib/game.rb
+++ ruby/memory_puzzle/lib/game.rb
@@ -7,16 +7,43 @@ class Game
   end
 
   def play
-    # todo
+    generate_board
+    until game_over
+      render_board
+      check_pair_of_cards
+    end
   end
 
   private
 
+  def render_board
+    @board.render
+  end
+
+  def check_pair_of_cards
+    # TODO
+  end
+
   def generate_board
     @board.populate
   end
-end
 
+  def game_over
+    @board.win?
+  end
+
+  def reveal_card
+    row, col = get_user_coordinates
+    @previous_guess = @board.reveal(row, col)
+  end
+
+  def get_user_coordinates
+    print  "Enter row and col numbers, separated by a comma:"
+    input = gets.chomp
+    # convert string "n, n" to number array [n, n]
+    input.split.map(&:to_i)
+  end
+end
 
 if __FILE__ == $PROGRAM_NAME
   Game.new.play
