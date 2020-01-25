b4a1ed0ee226f184bc72be2a41953fe967023ee7
jrupinski <rupinskijakub@gmail.com>
2020-01-25 20:20:34 +0100

    Privatize methods, add #remove_from_known_cards

--- ruby/memory_puzzle/lib/ai_player.rb
+++ ruby/memory_puzzle/lib/ai_player.rb
@@ -5,11 +5,28 @@ class AiPlayer
   end
 
   def receive_revealed_card(value, position)
-    @known_cards[value] << position unless @known_cards[value].include?(position)
+    add_received_card(value, position)
   end
 
   def receive_match(card_1_position, card_2_position)
+    add_matched_cards(card_1_position, card_2_position)
+  end
+
+  private
+
+  def add_received_card(value, position)
+    unless @known_cards[value].include?(position)
+      @known_cards[value] << position 
+    end
+  end
+
+  def add_matched_cards(card_1_position, card_2_position)
     @matched_cards.add(card_1_position)
     @matched_cards.add(card_2_position)
+    remove_from_known_cards(card_1_position, card_2_position)
+  end
+
+  def remove_from_known_cards(card_1_position, card_2_position)
+    @known_cards.delete_if { |k, v| v == card_1_position || card_2_position }
   end
 end
