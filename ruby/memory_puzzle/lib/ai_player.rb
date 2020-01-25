b2af8e9c6a9ffe58853bce541e0a03c909a8966c
jrupinski <rupinskijakub@gmail.com>
2020-01-25 20:07:17 +0100

    Add #receive_match

--- ruby/memory_puzzle/lib/ai_player.rb
+++ ruby/memory_puzzle/lib/ai_player.rb
@@ -1,10 +1,15 @@
 class AiPlayer
   def initialize
     @known_cards = Hash.new { |hash, key| hash[key] = Array.new }
+    @matched_cards = Set.new
   end
 
   def receive_revealed_card(value, position)
     @known_cards[value] << position unless @known_cards[value].include?(position)
   end
 
+  def receive_match(card_1_position, card_2_position)
+    @matched_cards.add(card_1_position)
+    @matched_cards.add(card_2_position)
+  end
 end
