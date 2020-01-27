5f4573b9d9466ae3ffdc491335e6749dd67b8cd5
jrupinski <rupinskijakub@gmail.com>
2020-01-27 03:45:30 +0100

    Add stub methods for HumanPlayer class

--- ruby/memory_puzzle/lib/human_player.rb
+++ ruby/memory_puzzle/lib/human_player.rb
@@ -13,4 +13,13 @@ class HumanPlayer
     # convert string "n, n" to number array [n, n]
     input.split.map(&:to_i)
   end
+
+  # STUB METHODS
+  def receive_revealed_card(value, position)
+    # do nothing
+  end
+  
+  def receive_match(card_1_position, card_2_position)
+    # do nothing
+  end
 end
