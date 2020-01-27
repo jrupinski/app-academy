e0329347b4d8c511e645c3a8c2879cf8303a43c7
jrupinski <rupinskijakub@gmail.com>
2020-01-27 04:02:49 +0100

    Fix Game#play with AI players only
    Why:
    - Add end-game notification about who won
    - Update Game#make_guess to update player's known cards list
    - Add #update_matched_cards, #update_known_cards

--- ruby/memory_puzzle/lib/game.rb
+++ ruby/memory_puzzle/lib/game.rb
@@ -6,7 +6,7 @@ class Game
   def initialize(size = 4)
     @board = Board.new(size)
     @previous_guess = nil
-    @players = [HumanPlayer.new, AiPlayer.new]
+    @players = [AiPlayer.new, AiPlayer.new]
     @current_player = 0
   end
 
@@ -16,16 +16,15 @@ class Game
       2.times do
         render_board
         position = current_player.guess
-        update_known_positions
         make_guess(position)
         clear_terminal
       end
-      
+
       next_player
     end
 
     render_board
-    puts "GAME OVER!"
+    puts "GAME OVER!\n #{current_player} WON!"
   end
 
   private
@@ -45,9 +44,12 @@ class Game
   def make_guess(position)
     reveal_card(position)
     current_card = position
-
+    update_known_cards(current_card)
+    
     if checking_another_card?
-      unless @board[@previous_guess] == @board[current_card]
+      if @board[@previous_guess] == @board[current_card]
+        update_matched_cards(@previous_guess, current_card)
+      else 
         render_board
         puts "try again"
         sleep 2
@@ -72,8 +74,15 @@ class Game
     system 'clear'
   end
 
-  def update_known_positions
-    # TODO
+  def update_matched_cards(card_1, card_2)
+    @players.each { |player| player.receive_match(card_1, card_2) }
+  end
+  
+  def update_known_cards(position)
+    @players.each do |player|
+      card_value = @board[position].value
+      player.receive_revealed_card(card_value, position)
+    end
   end
   
   def current_player
