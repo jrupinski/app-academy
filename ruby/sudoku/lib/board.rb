9058b8de0b9c104147fbd77bfcf93217469c08dc
jrupinski <rupinskijakub@gmail.com>
2020-01-28 02:46:37 +0100

    Add Board#initialize, Board::from_file

---
+++ ruby/sudoku/lib/board.rb
@@ -0,0 +1,13 @@
+require_relative "tile.rb"
+
+class Board
+  def initialize(grid)
+    @grid = Board.from_file(grid)
+  end
+
+  def self.from_file(filename)
+    file_data = File.read("../puzzles/#{filename}").split
+    file_array = file_data.map(&:chars)
+    file_array.map { |row| row.map { |value| Tile.new(value, false) } }
+  end
+end
