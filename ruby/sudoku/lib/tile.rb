9211db9e367e0b43ad10eac8bb73aa0f334edee7
jrupinski <rupinskijakub@gmail.com>
2020-01-28 01:49:24 +0100

    Add Tile#initialize, #to_s, #value=, #tile_given?

---
+++ ruby/sudoku/lib/tile.rb
@@ -0,0 +1,22 @@
+require 'colorize'
+
+class Tile
+  def initialize(value = nil, given = true)
+    @value = value
+    @given = given
+  end
+
+  def to_s
+    tile_given? ? @value.to_s.colorize(:red) : @value.to_s.colorize(:blue)
+  end
+  
+  def value=(number)
+    tile_given? ? @value = number : false
+  end
+
+  private
+
+  def tile_given?
+    @given
+  end
+end
