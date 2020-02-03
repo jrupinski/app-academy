e7947671e57d2da504c2018f0ddf76085ec37cbd
jrupinski <rupinskijakub@gmail.com>
2020-02-03 18:10:24 +0100

    Add Game#get_position and it's helper methods
    Why:
    - Adjust #play and #initialize
    - Add #valid_position?
    - Add #parse_position
    - Add #user_input
    - Add #board

--- ruby/sudoku/lib/game.rb
+++ ruby/sudoku/lib/game.rb
@@ -1,17 +1,41 @@
 require_relative "board.rb"
 
 class Game
-  attr_reader :board
-
-  def initialize(grid_size)
-    @board = Board.new(grid_size)
+  def initialize(filename)
+    @board = Board.new(filename)
   end
 
   def play
     until board.solved?
       board.render
-      prompt
-      board.set_value(get_user_input)
+      position = get_positon
+      puts "POSITION VALID!"
+      value = get_value
     end
+  end
+
+  def get_positon
+    print "Enter row and column position, separated by a comma:> "
+    loop do
+      pos = parse_position(user_input)
+      return pos if valid_position?(pos)
+      print "Position invalid or out of bounds. Retry:> "
+    end
+  end
+
+  def valid_position?(position)
+    position.count == 2 && position.all? { |value| value >= 0 && value < board.size }
+  end
+
+  def parse_position(input)
+    input.split(",").map(&:to_i)
+  end
+
+  def user_input
+    gets.chomp
+  end
+
+  def board
+    @board.grid
   end
 end
