42ca2a6e1fea260d19c96e427dc7b2c4e11dfe2e
jrupinski <rupinskijakub@gmail.com>
2020-02-05 19:13:26 +0100

    Add base methods of Ruby Recursion Homework

---
+++ ruby/recursion_homework/homework.rb
@@ -0,0 +1,30 @@
+# App Academy
+# RUBY section
+# Recursion Homework
+
+# Write a function sum_to(n) that uses recursion to calculate the sum from 1 to n (inclusive of n).
+def sum_to
+  # TODO
+end
+
+# Write a function add_numbers(nums_array) that takes in an array of Integers and returns the sum of those numbers.
+def add_numbers
+  # TODO
+end
+
+# Let's write a method that will solve Gamma Function recursively.
+# The Gamma Function is defined Γ(n) = (n-1)!. 
+def solve_gamma_function
+  # TODO
+end
+
+# Write a function ice_cream_shop(flavors, favorite) that takes in an array of ice cream flavors available at the ice cream shop, as well as the user's favorite ice cream flavor.
+# Recursively find out whether or not the shop offers their favorite flavor.
+def ice_cream_shop
+  # TODO
+end
+
+# Write a function reverse(string) that takes in a string and returns it reversed.
+def reverse
+  # TODO
+end
