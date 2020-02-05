7b519060e5f68634a24062a0ff6c9f8c91b3cea7
jrupinski <rupinskijakub@gmail.com>
2020-02-05 19:18:14 +0100

    Implement #sum_to

--- ruby/recursion_homework/homework.rb
+++ ruby/recursion_homework/homework.rb
@@ -3,8 +3,9 @@
 # Recursion Homework
 
 # Write a function sum_to(n) that uses recursion to calculate the sum from 1 to n (inclusive of n).
-def sum_to
-  # TODO
+def sum_to(n)
+  return n if n <= 1
+  n + sum_to(n - 1)
 end
 
 # Write a function add_numbers(nums_array) that takes in an array of Integers and returns the sum of those numbers.
