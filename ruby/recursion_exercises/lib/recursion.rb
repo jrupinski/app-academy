3178d680710764d2a403f7565e95290b8346e34d
jrupinski <rupinskijakub@gmail.com>
2020-02-06 11:24:16 +0100

    Implement ::range_iterative, change name of ::range
    Why:
    - Change name to ::range_recursive

--- ruby/recursion_exercises/lib/recursion.rb
+++ ruby/recursion_exercises/lib/recursion.rb
@@ -2,9 +2,12 @@ class Recursion
   # Warmup
   # Write a recursive method, range, that takes a start and an end and returns an array of all numbers in that range, exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. If end < start, you can return an empty array.
   # Write both a recursive and iterative version of sum of an array.
-
-  def self.range(start, end_range)
+  def self.range_recursive(start, end_range)
     return [] if end_range < start || start == end_range
-    [start] + range(start + 1, end_range)
+    [start] + range_recursive(start + 1, end_range)
+  end
+
+  def self.range_iterative(start, end_range)
+    (start...end_range).to_a
   end
 end
