02c1341dbf5e42bd543c85bd56b9b0136cb6548e
jrupinski <rupinskijakub@gmail.com>
2020-02-06 11:21:25 +0100

    Implement ::range

--- ruby/recursion_exercises/lib/recursion.rb
+++ ruby/recursion_exercises/lib/recursion.rb
@@ -3,7 +3,8 @@ class Recursion
   # Write a recursive method, range, that takes a start and an end and returns an array of all numbers in that range, exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. If end < start, you can return an empty array.
   # Write both a recursive and iterative version of sum of an array.
 
-  def self.range(start, end)
-    # TODO
+  def self.range(start, end_range)
+    return [] if end_range < start || start == end_range
+    [start] + range(start + 1, end_range)
   end
 end
