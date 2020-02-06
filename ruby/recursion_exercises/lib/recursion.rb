e25529fd0ba9a38addaf97e6dcfd7d9c2990d22f
jrupinski <rupinskijakub@gmail.com>
2020-02-06 11:34:44 +0100

    Implement first version of exponentaion

--- ruby/recursion_exercises/lib/recursion.rb
+++ ruby/recursion_exercises/lib/recursion.rb
@@ -1,5 +1,7 @@
 class Recursion
   # Warmup
+
+  # Range
   # Write a recursive method, range, that takes a start and an end and returns an array of all numbers in that range, exclusive. For example, range(1, 5) should return [1, 2, 3, 4]. If end < start, you can return an empty array.
   # Write both a recursive and iterative version of sum of an array.
   def self.range_recursive(start, end_range)
@@ -9,5 +11,26 @@ class Recursion
 
   def self.range_iterative(start, end_range)
     (start...end_range).to_a
+  end
+
+  # Exponentiation
+  # Write two versions of exponent that use two different recursions:
+  # Note that for recursion 2, you will need to square the results of exp(b, n / 2) and (exp(b, (n - 1) / 2). Remember that you don't need to do anything special to square a number, just calculate the value and multiply it by itself. So don't cheat and use exponentiation in your solution.
+
+  # recursion 1
+  # exp(b, 0) = 1
+  # exp(b, n) = b * exp(b, n - 1)
+  def self.exp_ver_1(b, n)
+    return 1 if n == 0
+    b * exp_ver_1(b, n - 1)
+  end
+
+  # recursion 2
+  # exp(b, 0) = 1
+  # exp(b, 1) = b
+  # exp(b, n) = exp(b, n / 2) ** 2             [for even n]
+  # exp(b, n) = b * (exp(b, (n - 1) / 2) ** 2) [for odd n]
+  def self.exp_ver_2
+    # TODO
   end
 end
