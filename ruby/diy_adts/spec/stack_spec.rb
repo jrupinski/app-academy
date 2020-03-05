2a462a1918421a02c53dc42a96b8edf663b4eb11
jrupinski <rupinskijakub@gmail.com>
2020-03-05 19:37:08 +0100

    Add Gemfile and a test suite for Stack

---
+++ ruby/diy_adts/spec/stack_spec.rb
@@ -0,0 +1,40 @@
+require "stack"
+
+describe "Stack" do
+  it "creates new Stack instance" do
+    expect { Stack.new }.to_not raise_error
+  end
+
+  let(:object1) { 1 }
+  let(:object2) { 2 }
+  let(:object3) { 3 }
+  
+  describe "It follows LIFO principle (last-in-first-out)" do
+    stack = Stack.new 
+
+    it "Adds new elements on top of stack" do
+      expect { stack.push(object1) }.to_not raise_error
+      stack.push(object2)
+      expect(stack.inspect.last).to eq(object2)
+      stack.push(object3)
+      expect(stack.inspect.last).to eq(object3)
+    end
+  
+    it "Removes top element from stack" do
+      expect { stack.pop }.to_not raise_error
+      expect(stack.pop).to eq(object2)
+      expect(stack.inspect).to eq([object1])
+    end
+  end
+
+  it "Allows peeking top element from the stack, without removing it" do
+    stack = Stack.new 
+
+    expect { stack.peek }.to_not raise_error
+    expect(stack.peek).to eq(nil)
+    stack.push(object3)
+    expect(stack.peek).to eq(object3)
+    stack.push(object2)
+    expect(stack.peek).to eq(object2)
+  end
+end
