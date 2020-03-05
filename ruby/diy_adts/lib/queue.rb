ad35647c026536bc698a2941f63aa1d84dd18f5e
jrupinski <rupinskijakub@gmail.com>
2020-03-05 23:43:25 +0100

    Implement Queue based on test suite

--- ruby/diy_adts/lib/queue.rb
+++ ruby/diy_adts/lib/queue.rb
@@ -1,5 +1,21 @@
 class Queue
-    def initialize
-        # TODO
-    end
+  def initialize
+    @queue = []
+  end
+
+  def inspect()
+    @queue
+  end
+
+  def enqueue(ele)
+    @queue.push(ele)
+  end
+
+  def dequeue
+    @queue.shift
+  end
+
+  def peek
+    @queue.first
+  end
 end
