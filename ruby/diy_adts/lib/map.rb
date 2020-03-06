72e585932fcf0e10dea3db07a21e88d818c56606
jrupinski <rupinskijakub@gmail.com>
2020-03-06 11:38:55 +0100

    Add Map

--- ruby/diy_adts/lib/map.rb
+++ ruby/diy_adts/lib/map.rb
@@ -1,5 +1,24 @@
 class Map
-    def initialize
-        # todo
-    end
+  def initialize
+    @map = []
+  end
+
+  def get(key)
+    pair = @map.select { |ele| ele.first == key }.flatten
+    pair.last
+  end
+
+  def set(key, value)
+    key_idx = @map.find_index { |(ele_key, ele_val)| ele_key == key }
+    key_idx.nil? ? @map.push([key, value]) : @map[key_idx].replace([key, value])
+  end
+
+  def delete(key)
+    key_idx = @map.find_index { |(ele_key, ele_val)| ele_key == key }
+    @map.delete_at(key_idx) unless key_idx.nil?
+  end
+
+  def show
+    @map
+  end
 end
