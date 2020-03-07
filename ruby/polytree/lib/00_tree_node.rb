6fff4efdf51e8164758fa2879226e8d6b4ca1914
jrupinski <rupinskijakub@gmail.com>
2020-03-07 11:42:38 +0100

    Add #initialize and #parent= to PolyTreeNode

--- ruby/polytree/lib/00_tree_node.rb
+++ ruby/polytree/lib/00_tree_node.rb
@@ -1,3 +1,28 @@
-class PolyTreeNode
+#
+# Create polytree Tree with n branch childen.
+#
 
+require "byebug"
+
+class PolyTreeNode
+  attr_reader :value, :parent, :children
+
+  def initialize(value)
+    @value = value 
+    @parent = nil
+    @children = []
+  end
+
+  #
+  # Change parent node. Add node to their parent's children and set it as new parent.
+  #
+  # @param [node] node new node
+  #
+  # @return [node] new parent node
+  #
+  def parent=(node)
+    parent.children.delete(self) unless parent.nil?
+    @parent = node
+    parent.children << self unless parent.nil? || parent.children.include?(self) 
+  end
 end
