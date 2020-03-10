f6d707df4ce81c5b149cc37bd73790f833e4f478
jrupinski <rupinskijakub@gmail.com>
2020-03-10 11:10:06 +0100

    Add framework for Knight Path Finder

---
+++ ruby/knight_path_finder/lib/knight_path_finder.rb
@@ -0,0 +1,9 @@
+#
+# Find the shortest path for a Chess Knight from a starting position to an end position.
+# Both the start and end positions should be on a standard 8x8 chess board.
+#
+class KnightPathFinder
+  def initialize(starting_position)
+    @position = starting_position
+  end
+end
