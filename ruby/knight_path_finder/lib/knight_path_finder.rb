4308bdb1505dc593a8594062500d7f07790df4f4
jrupinski <rupinskijakub@gmail.com>
2020-03-11 10:28:15 +0100

    Refactor KnightPathFinder
    Why:
    - Remove unnecessary instance variables
    - Rename DELTAS to MOVES, making code more coherent
    - Refactor ::valid_moves
    - Refactor #build_move_tree, remove tree output from it because move tree is stored in root_node already

--- ruby/knight_path_finder/lib/knight_path_finder.rb
+++ ruby/knight_path_finder/lib/knight_path_finder.rb
@@ -1,34 +1,28 @@
 require_relative "polytree_node"
 
-DELTAS = [
-  [-2, 1],
-  [-2, -1],
-  [2, 1],
-  [2, -1],
-  [1, 2],
-  [1, -2],
-  [-1, 2],
-  [-1, -2]
-].freeze
-
 #
 # Find the shortest path for a Chess Knight from a starting position to an end position.
 # Both the start and end positions should be on a standard 8x8 chess board.
 #
 class KnightPathFinder
-  attr_reader :root_node
-  
-  def initialize(starting_position)
-    @board = Array.new(8) { Array.new(8) }
-    @position = starting_position
-    @root_node = self.root_node
-    @considered_positions = [starting_position]
+
+  MOVES = [
+    [-2, 1],
+    [-2, -1],
+    [2, 1],
+    [2, -1],
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2]
+  ].freeze
+
+  def initialize(start_pos)
+    @start_pos = start_pos
+    @considered_positions = [start_pos]
+    build_move_tree
   end
 
-  def root_node
-    PolyTreeNode.new(@position)
-  end
-  
   #
   # Get all valid chess knight moves from given position
   #
@@ -36,15 +30,23 @@ class KnightPathFinder
   #
   # @return [Array] Valid moves for Chess knight to make
   #
-  def valid_moves(pos)
-    DELTAS.map do |dx, dy|
-      [pos[0] + dx, pos[1] + dy]
-    end.select do |row, col|
-      [row, col].all? do |coord|
-        coord.between?(0, 8) # board size is 8x8
+  def self.valid_moves(pos)
+    valid_moves = []
+    
+    cur_x, cur_y = pos
+    MOVES.each do |(dx, dy)|
+      new_pos = [cur_x + dx, cur_y + dy]
+      if new_pos.all?{ |coord| coord.between?(0, 7) } # chess board is 8x8
+        valid_moves << new_pos
       end
     end
+
+    valid_moves
   end
+
+  private
+
+  attr_accessor :root_node
 
   #
   # Get valid moves from given position that have not been visitied/considered yet.
@@ -54,7 +56,7 @@ class KnightPathFinder
   # @return [Array] Array of possible moves/positions
   #
   def new_move_positions(pos)
-    new_positions = valid_moves(pos) - @considered_positions
+    new_positions = KnightPathFinder.valid_moves(pos) - @considered_positions
     @considered_positions += new_positions
     new_positions
   end
@@ -62,27 +64,19 @@ class KnightPathFinder
   #
   # Build a move tree in a breadth-first manner
   #
-  # @return [Array] Tree of possible moves 
-  #
   def build_move_tree
-    tree = []
+    self.root_node = PolyTreeNode.new(@start_pos)
 
-    queue = [root_node]
-
-    until queue.empty?
-      current_node = queue.shift
+    nodes = [root_node]
+    until nodes.empty?
+      current_node = nodes.shift
       current_pos = current_node.value
 
-      possible_nodes = new_move_positions(current_pos).map do |pos|
-        new_node = PolyTreeNode.new(pos)
-        new_node.parent = current_node
-        new_node
+      new_move_positions(current_pos).each do |pos|
+        next_node = PolyTreeNode.new(pos)
+        current_node.add_child(next_node)
+        nodes << next_node
       end
-      
-      tree += possible_nodes
-      queue += possible_nodes
     end
-
-    tree
   end
 end
