910768d8612189b3e311f27c6192d5075ace3571
jrupinski <rupinskijakub@gmail.com>
2020-03-12 11:46:50 +0100

    Add children node generation to TicTacToeNode

--- ruby/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ ruby/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -1,7 +1,12 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
+
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +18,29 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    possible_nodes
+  end
+  
+  private
+
+  def possible_nodes
+    possible_nodes = []
+  
+    (0...board.rows.count).each do |row|
+      (0...board.rows.count).each do |col|
+        pos = [row, col]
+        opposite_mark = self.next_mover_mark == :x ? :o : :x
+        
+        if board.empty?(pos)
+          board_after_move = board.dup
+          board_after_move[pos] = opposite_mark
+          next_node = TicTacToeNode.new(board_after_move, opposite_mark, pos)
+
+          possible_nodes << next_node
+        end
+      end
+    end
+  
+    possible_nodes
   end
 end
