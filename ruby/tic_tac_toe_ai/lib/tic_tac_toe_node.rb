bae5d1a6da7ed9a1fa1c9c59ab30bd6f0bf38151
jrupinski <rupinskijakub@gmail.com>
2020-03-14 19:05:49 +0100

    Fix bug in #children, add #losing_node?
    Why:
    - Remove #possible_nodes and move it's logic into #children
    - Fix bug in #children that assigned opposite marks to every node, which made

--- ruby/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ ruby/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -1,5 +1,6 @@
 require_relative 'tic_tac_toe'
 
+require "byebug"
 class TicTacToeNode
   attr_reader :board, :next_mover_mark, :prev_move_pos
 
@@ -10,37 +11,41 @@ class TicTacToeNode
   end
 
   def losing_node?(evaluator)
+    if board.over?
+      return board.won? && board.winner != evaluator
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.all? { |node| node.losing_node?(evaluator) }
+    else
+      self.children.any? { |node| node.losing_node?(evaluator) }
+    end
   end
 
   def winning_node?(evaluator)
+    # TODO
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
-    possible_nodes
-  end
-  
-  private
-
-  def possible_nodes
-    possible_nodes = []
+    children = []
   
     (0...board.rows.count).each do |row|
       (0...board.rows.count).each do |col|
         pos = [row, col]
         opposite_mark = self.next_mover_mark == :x ? :o : :x
-        
+
         if board.empty?(pos)
           board_after_move = board.dup
-          board_after_move[pos] = opposite_mark
+          board_after_move[pos] = self.next_mover_mark
           next_node = TicTacToeNode.new(board_after_move, opposite_mark, pos)
 
-          possible_nodes << next_node
+          children << next_node
         end
       end
     end
   
-    possible_nodes
+    children
   end
 end
