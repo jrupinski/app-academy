9eb01fe8e21a6c2308b258fd404377db9f2e5ec2
jrupinski <rupinskijakub@gmail.com>
2020-03-15 14:49:22 +0100

    Add #winning_node?, add/improve documentation

--- ruby/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
+++ ruby/tic_tac_toe_ai/lib/tic_tac_toe_node.rb
@@ -1,6 +1,5 @@
 require_relative 'tic_tac_toe'
 
-require "byebug"
 class TicTacToeNode
   attr_reader :board, :next_mover_mark, :prev_move_pos
 
@@ -10,6 +9,15 @@ class TicTacToeNode
     @prev_move_pos = prev_move_pos
   end
 
+  #
+  # Check if node is a losing node (if this move will end up in given player losing)
+  # If it's player's turn - check if all possible moves will end up in losing
+  # If it's opponent's turn - check if any of his moves will end up in player losing
+  #
+  # @param [Symbol] evaluator Symbol of player
+  #
+  # @return [Boolean] Is this node a losing node for player
+  #
   def losing_node?(evaluator)
     if board.over?
       return board.won? && board.winner != evaluator
@@ -22,12 +30,32 @@ class TicTacToeNode
     end
   end
 
+  #
+  # Check if node is a winning node (if this move is a winning move)
+  # If it's player's turn - check if there's any winning moves possible
+  # If it's opponent's turn - check if all his moves will end up in player winning
+  #
+  # @param [Symbol] evaluator Symbol of player
+  #
+  # @return [Boolean] Is this node a winning node for player
+  #
   def winning_node?(evaluator)
-    # TODO
+    if board.over?
+      return board.won? && board.winner == evaluator
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.any? { |node| node.winning_node?(evaluator) }
+    else
+      self.children.all? { |node| node.winning_node?(evaluator) }
+    end
   end
 
-  # This method generates an array of all moves that can be made after
-  # the current move.
+  #
+  # Generate an array of possible moves one turn ahead
+  #
+  # @return [Array] Array of Nodes (possible moves)
+  #
   def children
     children = []
   
