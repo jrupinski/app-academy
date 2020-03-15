7e9ba569b6bcef3d141d1f40e546b8f6819603d0
jrupinski <rupinskijakub@gmail.com>
2020-03-15 15:52:15 +0100

    Implement SuperComputerPlayer that only wins/ties
    Why:
    Implement #move that uses TicTacToeNode class (a Polytree)

--- ruby/tic_tac_toe_ai/lib/super_computer_player.rb
+++ ruby/tic_tac_toe_ai/lib/super_computer_player.rb
@@ -1,7 +1,33 @@
 require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
+  #
+  # Hard AI player that only wins or ties.
+  # Upgraded ComputerPlayer that only takes winning or non-losing move.
+  # Stores possible moves in a Polytree to see one move ahead.
+  #
+  # @param [Game] game Instance of Tic Tac Toe game
+  # @param [Symbol] mark Mark given to AI player
+  #
+  # @return [Array] Position of next move (winning or non-losing one)
+  #
   def move(game, mark)
+    current_node = TicTacToeNode.new(game.board, mark)
+    possible_moves = current_node.children
+    losing_moves = []
+
+    # go to first possible winning node, else collect list of loser nodes
+    possible_moves.each do |node|
+      return node.prev_move_pos if node.winning_node?(mark)
+      losing_moves << node if node.losing_node?(mark)
+    end
+
+    possible_moves = possible_moves - losing_moves
+    raise "No non-losing moves left!" if possible_moves.empty?
+
+    
+    # return random move that isn't a losing one
+    possible_moves.sample.prev_move_pos
   end
 end
 
