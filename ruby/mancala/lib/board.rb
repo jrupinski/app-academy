7e788f47fbc2e5e10f140e809d38285432995698
jrupinski <rupinskijakub@gmail.com>
2020-03-18 13:31:02 +0100

    Add #initialize and #place_stones to Board class

--- ruby/mancala/lib/board.rb
+++ ruby/mancala/lib/board.rb
@@ -1,14 +1,27 @@
 class Board
+  STORE_CUPS = [6, 13].freeze
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { [] }
+    place_stones
   end
 
+  # helper method to #initialize every non-store cup with four stones each
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    cups.each_with_index do |cup, cup_idx|
+      4.times { cup << :stone } unless STORE_CUPS.include?(cup_idx) 
+    end
   end
 
   def valid_move?(start_pos)
+    if !start_pos.between?(0, cups.count) || STORE_CUPS.include?(start_pos) 
+      raise "Invalid starting cup"
+    elsif cups[start_pos].empty?
+      raise "Starting cup is empty"
+    end
+
+    true
   end
 
   def make_move(start_pos, current_player_name)
