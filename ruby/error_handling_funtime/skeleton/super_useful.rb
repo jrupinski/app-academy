de90e4844a5e95d5202a18370e8f5adeecc2e805
jrupinski <rupinskijakub@gmail.com>
2020-03-23 21:11:52 +0100

    Add error handling based on exercise guidelines

--- ruby/error_handling_funtime/skeleton/super_useful.rb
+++ ruby/error_handling_funtime/skeleton/super_useful.rb
@@ -1,33 +1,73 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+  rescue ArgumentError
+    puts "This is not a valid number"
+    nil
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
+class NotAFruitError < StandardError
+  def message
+    "This is not a fruit. Conversation over."
+  end
+end
+
+class CoffeeError < StandardError
+  def message
+    "Thanks for coffee, I really needed it. Go ahead, try again."
+  end
+end
+
+
 def reaction(maybe_fruit)
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit.downcase == "coffee"
+    raise CoffeeError
   else 
-    raise StandardError 
+    raise NotAFruitError 
   end 
 end
 
 def feed_me_a_fruit
   puts "Hello, I am a friendly monster. :)"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue CoffeeError => e
+    puts e.message
+    retry
+  rescue NotAFruitError => e
+    puts e.message
+  end
 end  
 
 # PHASE 4
+# class FriendshipTooShortError < StandardError
+#   def message
+#     "Friendship too short. Minimum duration: 5 years."
+#   end
+# end
+
+# class NameMissingError < StandardError; end
+# class FavPastimeMissingError < StandardError; end
+
+
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
-    @name = name
+    raise ArgumentError.new("Name cannot be empty!") if name.empty?
+    raise ArgumentError.new("Friendship too short. Minimum duration: 5 years.") if yrs_known < 5
+    raise ArgumentError.new("Favorite pastime cannot be empty!") if fav_pastime.empty?
+
+    @name = name 
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
+    
   end
 
   def talk_about_friendship
--- ruby/error_handling_funtime/skeleton/user_script.rb
+++ ruby/error_handling_funtime/skeleton/user_script.rb
@@ -4,7 +4,7 @@ puts "'five' == #{convert_to_int('five')}"
 
 feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('Gyro', 5, 'Mastering the spinning technique')
 
 sam.talk_about_friendship
 sam.do_friendstuff
