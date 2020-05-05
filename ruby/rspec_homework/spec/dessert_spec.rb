c6254ee5c2f34f7bc688c6684ca5f8b2e1ebcb60
jrupinski <rupinskijakub@gmail.com>
2020-05-05 12:31:04 +0200

    Implement Rspec file, add Guard-rpsec auto testing

--- ruby/rspec_homework/spec/dessert_spec.rb
+++ ruby/rspec_homework/spec/dessert_spec.rb
@@ -6,37 +6,67 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
+  subject(:panna_cotta) { Dessert.new("Panna Cotta", 3, chef) }
   let(:chef) { double("chef") }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      expect(panna_cotta.type).to eq("Panna Cotta")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      expect(panna_cotta.quantity).to eq(3)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(panna_cotta.ingredients).to eq([])
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect { panna_cotta }.to_not raise_error
+      expect { Dessert.new("Pizza", "a lot", chef) }.to raise_error(ArgumentError)
+    endchef
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      panna_cotta.add_ingredient("milk")
+      panna_cotta.add_ingredient("sugar")
+      expect(panna_cotta.ingredients).to include("milk")
+      expect(panna_cotta.ingredients).to include("sugar")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      expect(panna_cotta.ingredients).to receive(:shuffle!)
+      panna_cotta.mix!
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      expect(panna_cotta.quantity).to eq(3)
+      panna_cotta.eat(2)
+      expect(panna_cotta.quantity).to eq(1)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { panna_cotta.eat(5) }.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      expect(chef).to receive(:titleize)
+      panna_cotta.serve
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(panna_cotta)
+      panna_cotta.make_more
+    end
   end
 end
