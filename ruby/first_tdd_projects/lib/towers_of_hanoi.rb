6ce6105e3385a874bb8e83352609d644b21d049c
jrupinski <rupinskijakub@gmail.com>
2020-05-14 18:38:59 +0200

    Add test specs for Hanoi Towers, implement it

--- ruby/first_tdd_projects/lib/towers_of_hanoi.rb
+++ ruby/first_tdd_projects/lib/towers_of_hanoi.rb
@@ -1,5 +1,71 @@
 class TowersOfHanoi
+  attr_reader :towers
+
   def initialize
-    # TODO
+    @towers = Array.new(3) { [] } 
+    @towers[0] += [3, 2, 1]
   end
+
+  def play
+    system "clear"
+    until won?
+      print_towers
+      move_disc
+    end
+
+    system "clear"
+    puts "GAME OVER!"
+  end
+
+  def move(start_tower, end_tower)
+    raise IndexError, "Invalid index of towers" unless [start_tower, end_tower].all? { |arg| arg.between?(0, towers.count - 1) }
+    raise ArgumentError, "Starting tower is empty!" if towers[start_tower].empty?
+    unless towers[end_tower].empty?
+      raise ArgumentError, "Cannot put bigger disk on a smaller one!" if towers[start_tower].last > towers[end_tower].last
+    end
+
+    move!(start_tower, end_tower)
+  end
+  
+  def won?
+    towers.last == [3, 2, 1] && towers[0..1].all?(&:empty?)
+  end
+
+  private
+
+  def move_disc
+    begin
+      start_tower, end_tower = user_input
+      move(start_tower, end_tower)
+    rescue => exception
+      puts "#{exception.message}. Try Again!"
+      sleep 2
+      retry
+    ensure
+      system "clear"
+    end
+  end
+
+  def user_input
+    puts "Enter tower(1, 2 or 3) to take disk from:> "
+    start_tower = gets.to_i - 1
+    puts "\nEnter tower(1, 2 or 3) to put disk on:> "
+    end_tower = gets.to_i - 1
+
+    [start_tower, end_tower]
+  end
+
+
+  def print_towers
+    puts "Towers: #{towers[0]} #{towers[1]} #{towers[2]}"
+  end
+
+  def move!(start_tower, end_tower)
+    @towers[end_tower] << @towers[start_tower].pop
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  game = TowersOfHanoi.new
+  game.play
 end
--- ruby/first_tdd_projects/spec/towers_of_hanoi_spec.rb
+++ ruby/first_tdd_projects/spec/towers_of_hanoi_spec.rb
@@ -1,14 +1,47 @@
 require "towers_of_hanoi"
 
 describe "Towers of Hanoi" do
-  it "Creates an instance of Tower of Hanoi"
-  
-  it "Moves only one disk at a time"
+  subject(:hanoi) { TowersOfHanoi.new }
 
-  it "Each move removes upper disk from one stack and moves it to another, or on an empty rod"
+  it "Creates an instance of Tower of Hanoi" do
+    expect { hanoi }.to_not raise_error
+    expect(hanoi.towers).to be_an(Array)
+    expect(hanoi.towers.all? { |tower| tower.is_a?(Array) }).to be true
+  end
 
-  it "Doesn't allow placing larger disks on top of a smaller disk"
+  it "Starts with only first tower populated" do 
+    expect(hanoi.towers.first).to_not be_empty
+    expect(hanoi.towers.one? { |tower| !tower.empty? }).to be true
+  end
+
+  it "Moves only one disk at a time" do
+    hanoi.move(0, 2)
+    expect(hanoi.towers).to eq([[3, 2], [], [1]])    
+  end
+
+  it "Doesn't allow placing larger disks on top of a smaller disk" do
+    # towers: [3], [1], [2]
+    hanoi.move(0, 1)
+    hanoi.move(0, 2)
+
+    expect { hanoi.move(2, 1) }.to raise_error(ArgumentError)
+    expect { hanoi.move(0, 1) }.to raise_error(ArgumentError)
+    expect { hanoi.move(1, 0) }.to_not raise_error
+    expect { hanoi.move(0, 2) }.to_not raise_error
+  end
+
+  it "Allows Only one disk to be moved at a time." do
+    expect { hanoi.move(-1, 1) }.to raise_error(IndexError)
+    expect { hanoi.move(0, 3) }.to raise_error(IndexError)
+    hanoi.move(0, 2)
+    expect(hanoi.towers).to eq([[3, 2], [], [1]])
+  end
+
+  it "Wins if all discs are on the last tower" do
+    expect(hanoi.won?).to be false
+    allow(hanoi).to receive(:towers).and_return([[], [3, 2, 1], []])
+    expect(hanoi.won?).to be false
+    allow(hanoi).to receive(:towers).and_return([[], [], [3, 2, 1]])
+    expect(hanoi.won?).to be true
+  end
 end
-# Only one disk can be moved at a time.
-    # Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack or on an empty rod.
-    # No larger disk may be placed on top of a smaller disk.
