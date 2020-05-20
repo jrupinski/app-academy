dcec4fa4645fd91349b72274445f563b9e9b9f75
jrupinski <rupinskijakub@gmail.com>
2020-05-20 16:10:46 +0200

    Implement Card#value and fix Card specs

--- ruby/poker/lib/card.rb
+++ ruby/poker/lib/card.rb
@@ -1,5 +1,28 @@
 class Card
-  def initialize
-    # todo
+
+  VALUE_HASH = {
+    "2" => 2,
+    "3" => 3,
+    "4" => 4,
+    "5" => 5,
+    "6" => 6,
+    "7" => 7,
+    "8" => 8,
+    "9" => 9,
+    "10" => 10,
+    "J" => 11,
+    "Q" => 12,
+    "K" => 13
+  }
+
+  def initialize(value)
+    @value = value
   end
+
+  def value
+    raise ArgumentError unless @value.length.between?(2, 3)
+    value_str = @value.chars.take(@value.length - 1).join
+    VALUE_HASH.has_key?(value_str) ? VALUE_HASH[value_str] : (raise ArgumentError)
+  end
+
 end
--- ruby/poker/spec/card_spec.rb
+++ ruby/poker/spec/card_spec.rb
@@ -9,29 +9,46 @@ describe Card do
   describe "#value" do
     context "Two of Hearts" do
       let(:card_type) { "2H" }
-      it { expect(:value).to eq 2 }
+      it { expect(subject.value).to eq 2 }
     end
-    
+
+    context "Ten of Spades" do
+      let(:card_type) { "10S" }
+      it { expect(subject.value).to eq 10 }
+    end
+
     describe "Face Cards" do
       context "Jack of Clubs" do
         let(:card_type) { "JC" }
-        it { expect(:value).to eq 11 }
+        it { expect(subject.value).to eq 11 }
       end
 
       context "Queen of Diamonds" do
         let(:card_type) { "QD" }
-        it { expect(:value).to eq 12 }
+        it { expect(subject.value).to eq 12 }
       end
 
       context "King of Spades" do
         let(:card_type) { "KS" }
-        it { expect(:value).to eq 13 }
+        it { expect(subject.value).to eq 13 }
       end
     end
 
-    context "bad value" do
-      let(:card_type) { "1S" }
-      it { is_expected.to raise_error(ArgumentError) }
+    context "Bad value" do
+      context "Bad string:" do
+        let(:card_type) { "1S" }
+        it { expect { subject.value }.to raise_error(ArgumentError) }
+      end
+
+      context "Array:" do
+        let(:card_type) { [10] }
+        it { expect { subject.value }.to raise_error(ArgumentError) }
+      end
+
+      context "Empty string:" do
+        let(:card_type) { "" }
+        it { expect { subject.value }.to raise_error(ArgumentError) }
+      end
     end
   end
 end
