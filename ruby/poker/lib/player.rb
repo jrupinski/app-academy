2a3d0103da979a5df6507a5b5ce229b04c40eb0b
jrupinski <rupinskijakub@gmail.com>
2020-06-09 14:34:42 +0200

    Improved specs, implemented Player

--- ruby/poker/lib/player.rb
+++ ruby/poker/lib/player.rb
@@ -1,5 +1,49 @@
+require "deck"
+
 class Player
-  def initialize
-    # TODO
+  attr_reader :pot, :hand
+  
+  def initialize(pot)
+    raise ArgumentError unless pot.is_a?(Integer)
+    @pot = pot
+    @hand = []
+  end
+
+  def draw!(card)
+    raise TypeError unless card.is_a?(Card)
+    raise MaxHandSizeError if hand.size >= 5
+    hand << card
+  end
+
+  def discard!
+    hand.delete_at(get_card_idx)
+  end
+
+  def fold
+    :fold
+  end
+
+  def see
+    :see
+  end
+
+  def raise_amount
+    get_raise_amount
+  end
+
+  private
+
+  def get_card_idx
+    Integer(gets)
+  end
+
+  def get_raise_amount
+    Integer(gets)
+  end
+end
+
+class MaxHandSizeError < StandardError
+  def initialize(msg="5 cards in hand limit reached")
+    super
   end
 end
--- ruby/poker/spec/player_spec.rb
+++ ruby/poker/spec/player_spec.rb
@@ -1,13 +1,72 @@
 require "player"
 
 describe "Player" do
-  it "Has a hand"
-  it "Has a pot"
+  subject(:player) { Player.new(1000) }
+  
+  # CARDS
+  let(:four) { double(:value => 4, :suit => "hearts") }
+  let(:five) { double(:value => 5, :suit => "clubs") }
+  # deck
+  let(:deck) { double(:cards => [four, five, four, five, four, five]) }
+
+  before(:each) do
+    allow(deck).to receive(:draw!).and_return(deck.cards.pop)
+    allow(deck).to receive(:array).and_return(deck.cards)
+    allow(deck).to receive(:is_a?).with(Class) { true }
+    allow(four).to receive(:is_a?).with(Class) { true }
+    allow(five).to receive(:is_a?).with(Class) { true }
+  end
+
+
+  context "Player's hand" do
+    it "starts with empty hand" do
+      expect(player.hand).to be_empty
+    end
+
+    it "holds 5 cards max" do
+      allow(player).to receive(:hand).and_return([five, four, five, four])
+
+      expect(player.hand.size).to eq(4)
+      player.draw!(five)
+      expect(player.hand.size).to eq(5)
+      
+      expect { player.draw! }.to raise_error(ArgumentError)
+      expect { player.draw!("bad data") }.to raise_error(TypeError)
+      expect { player.draw!(deck.draw!) }.to raise_error(MaxHandSizeError)
+    end
+  end
+
+  it "Has a pot" do
+    expect(player.pot).to be_kind_of(Integer)
+    expect { Player.new("test") }.to raise_error(ArgumentError)
+  end
 
   context "Gameplay methods" do
-    it "Allows the player to select cards to discard"
-    it "Allows the player to fold"
-    it "Allows the player to see"
-    it "Allows the player to raise"
+    before(:each) do
+        allow(player).to receive(:hand).and_return([five, four, five, four, five])
+    end
+
+    it "Allows the player to select cards to discard" do
+      allow(player).to receive(:gets).and_return(0)
+
+      expect(player.hand.size).to eq(5)
+      expect(player.discard!).to eq(five)
+      expect(player.hand.size).to eq(4)
+    end
+
+    it "Allows the player to fold" do
+      expect(player.fold).to eq(:fold)
+    end
+
+    it "Allows the player to see" do
+      expect(player.see).to eq(:see)
+    end
+
+    it "Allows the player to raise" do
+      allow(player).to receive(:gets).and_return 100
+      expect(player.raise_amount).to eq(100)
+      allow(player).to receive(:gets).and_return "bad_data"
+      expect { player.raise_amount }.to raise_error(ArgumentError)
+    end
   end
 end
