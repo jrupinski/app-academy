24a752e54c66ee875294dffa7e71e57f025c9741
jrupinski <jakubrupinski@gmail.com>
2020-06-25 00:18:20 +0200

    Add O(n) and O(1) tentacle finding methods for Octopus

--- ruby/octopus_problems/lib/octopus.rb
+++ ruby/octopus_problems/lib/octopus.rb
@@ -2,6 +2,43 @@ class Octopus
   FISHES = ['fish', 'fiiish', 'fiiiiish', 'fiiiish',
    'fffish', 'ffiiiiisshh', 'fsh', 'fiiiissshhhhhh']
 
+   TILES_ARRAY = [
+     "up", "right-up", "right", "right-down", "down", "left-down", "left",
+      "left-up" 
+    ]
+
+    TILES_HASH = {
+      "up" => 0,
+      "right-up" => 1,
+      "right" => 2,
+      "right-down" => 3,
+      "down" => 4,
+      "left-down" => 5,
+      "left" => 6,
+      "left-up" => 7
+    }
+
+  #
+  # Given a tile direction, iterate through a tiles array to return
+  # the tentacle number (tile index) the octopus must move.
+  # This should take O(n) time.
+  #
+  def self.slow_dance(direction, dance_floor = TILES_ARRAY)
+    raise ArgumentError unless TILES_ARRAY.include?(direction)
+    TILES_ARRAY.each_with_index do |tile, tentacle_idx|
+      return tentacle_idx if tile == direction
+    end
+  end
+
+
+  #
+  # Use a different data structure and write a new function so that you can
+  # access the tentacle number in O(1) time.
+  #
+  def self.constant_dance(direction, dance_floor = TILES_HASH)
+    raise ArgumentError unless TILES_ARRAY.include?(direction)
+    TILES_HASH[direction]
+  end
   # Find the longest fish in O(n^2) time. Do this by comparing all fish lengths to all other fish lengths
   def self.sluggish_octopus
     longest_fish = ""
@@ -78,3 +115,16 @@ puts "O(n log n) time:"
 puts Octopus.dominant_octopus
 puts "O(n) time:"
 puts Octopus.clever_octopus
+
+puts
+puts "Dancing octopus!"
+puts "Slow dance:"
+puts Octopus.slow_dance("up")
+# > 0
+puts Octopus.slow_dance("right-down")
+# > 3
+puts "Constant dance:"
+puts Octopus.constant_dance("up")
+# > 0
+puts Octopus.constant_dance("right-down")
+# > 3
