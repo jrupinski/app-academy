e6b3fc89f86a102fb94142205d23f35ef4b1d9e8
jrupinski <jakubrupinski@gmail.com>
2020-08-21 19:51:21 +0200

    Add LRUCache test suite and generate files for it

---
+++ ruby/lru_cache/spec/lru_cache_spec.rb
@@ -0,0 +1,45 @@
+require "rspec"
+require "lru_cache"
+
+describe "LRU Cache" do
+  subject(:johnny_cache) { LRUCache.new(4) }
+
+  it "creates a LRU Cache" do
+    expect { LRUCache.new(4) }.to_not raise_error
+  end
+
+
+  it "adds elements to the cache" do
+    expect(johnny_cache.add("I walk the line")).to_not raise_error
+    expect(johnny_cache.add(5)).to_not raise_error
+  end
+  
+  it "allows to return cache item count" do
+    expect(LRUCache.new.count).to eq(0)
+    expect(johnny_cache.count).to eq(2)
+
+    johnny_cache.add([1,2,3])
+    johnny_cache.add(5)
+    expect(johnny_cache.count).to eq(4)
+
+    johnny_cache.add(-5)
+    johnny_cache.add({a: 1, b: 2, c: 3})
+    expect(johnny_cache.count).to eq(4)
+  end
+  
+  it "allows to print the cache items" do
+    johnny_cache.add("this should be gone from cache")
+    johnny_cache.add([1,2,3,4])
+    johnny_cache.add("I walk the line")
+    johnny_cache.add(:ring_of_fire)
+    johnny_cache.add("I walk the line")
+    johnny_cache.add({a: 1, b: 2, c: 3})
+    
+    expect(johnny_cache.show).to eq (
+      [[1, 2, 3, 4], 
+      :ring_of_fire, 
+      "I walk the line", 
+      {:a=>1, :b=>2, :c=>3}]
+      )
+  end
+end
