f6dac4245c9a314daeb74fe0efbdce765c6cec3d
jrupinski <jakubrupinski@gmail.com>
2020-08-21 19:17:12 +0200

    Remove linked list version of cache

--- ruby/lru_cache/lib/doubly_linked_list.rb
+++
@@ -1,9 +0,0 @@
-class DoublyLinkedList
-  attr_accessor :prev_node, :next_node, :val
-
-  def initialize(val = nil, next_node = nil, prev_node = nil)
-    @val = val
-    @next_node = next_node
-    @prev_node = prev_node
-  end
-end
