1d99c57bf7fc49c56ea64a52bf1fc810d307c340
jrupinski <jakubrupinski@gmail.com>
2020-09-02 20:56:20 +0200

    Fix wrong name in String#hash, implement Hash#hash

--- ruby/my_hash_map/lib/p02_hashing.rb
+++ ruby/my_hash_map/lib/p02_hashing.rb
@@ -5,16 +5,20 @@ end
 class Array
   def hash
     self
-    .map.with_index { |ele, idx| ele.to_i ^ idx }
-    .sum
-    .hash
+      # use XOR of each element and it's index as base for hashing
+      .flatten
+      .map.with_index { |ele, idx| ele.to_i ^ idx }
+      .sum
+      .hash
   end
 end
 
 class String
   def hash
     bytes = []
-    self.each_byte { |char| hashed << char }
+    # convert each char to bytes, and then hash that array of bytes
+    self.each_byte { |char| bytes << char }
+    # hash the byte array
     bytes.hash
   end
 end
@@ -23,6 +27,13 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    # add keys and values to a single array
+    hash_as_array = self.keys + self.values
+    # convert any symbols in Array into a string
+    hash_as_array.map! { |ele| ele.to_s }
+    # sort keys and values to make the hash order-agnostic
+    hash_as_array.sort!
+    # hash the array of keys and values
+    hash_as_array.hash
   end
 end
