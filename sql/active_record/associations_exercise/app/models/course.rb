f4927221a318ae6eebac6140dc53501535041e5a
jrupinski <rupinskijakub@gmail.com>
2020-12-01 11:22:19 +0100

    Add User, Enrollment and Course associations
    Why:
    - Add courses and enrollments to User
    - Add users and courses to Enrollment
    - Add enrolled_students, enrollments, prerequisite and teacher to Course

--- sql/active_record/associations_exercise/app/models/course.rb
+++ sql/active_record/associations_exercise/app/models/course.rb
@@ -1,2 +1,36 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  prereq_id     :integer
+#  instructor_id :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user,
+    primary_key: :id,
+    foreign_key: :course_id
+
+  belongs_to :prerequisite,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course,
+    optional: true
+
+  belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User,
+    optional: true
 end
--- sql/active_record/associations_exercise/app/models/user.rb
+++ sql/active_record/associations_exercise/app/models/user.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+  
+  has_many :enrollments,
+    primary_key: :id,  # Enrollment's ID
+    foreign_key: :student_id,  # Student's ID
+    class_name: :Enrollment
+
+  has_many :enrolled_courses,
+    through: :enrollments,
+    source: :course
 end
