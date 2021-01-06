c94fd0289269baa90120b7ade765abde59037f8e
jrupinski <rupinskijakub@gmail.com>
2021-01-06 21:34:10 +0100

    Implement #better_tracks_query in 3 SQL queries
    Why:
    - Implement #better_tracks_query
    - Annotate every model using Annotate gem
    - Add annotate and pry-rails gems to Gemfile

--- sql/active_record_2/n_plus_1_query_buster/app/models/artist.rb
+++ sql/active_record_2/n_plus_1_query_buster/app/models/artist.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Artist < ApplicationRecord
   has_many :albums,
     class_name: 'Album',
@@ -15,6 +24,17 @@ class Artist < ApplicationRecord
   end
 
   def better_tracks_query
-    # TODO: your code here
+    # prefetch tracks without any data
+    albums = self.albums.includes(:tracks)
+    tracks_count = {}
+
+    albums.each do |album|
+      # i use #length to avoid rails firing up another query, because data
+      # is already prefetched in #includes(:tracks)
+      tracks_count[album.title] = album.tracks.length
+    end
+
+    tracks_count
+    # 3 queries total 
   end
 end
