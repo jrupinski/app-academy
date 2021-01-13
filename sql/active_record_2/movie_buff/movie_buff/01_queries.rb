64d9c24039c478ced791f0b44cc364a9321ea559
jrupinski <rupinskijakub@gmail.com>
2021-01-13 20:19:36 +0100

    Implement ActiveRecord #harrison_ford query

--- sql/active_record_2/movie_buff/movie_buff/01_queries.rb
+++ sql/active_record_2/movie_buff/movie_buff/01_queries.rb
@@ -23,6 +23,14 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+  Movie
+    .select(:id, :title)
+    .joins(:actors, :castings)
+    .where(actors: { name: 'Harrison Ford' })
+    .where.not(castings: { ord: 1 })
+    # ver. 2, pure SQL
+    # .where('actors.name = \'Harrison Ford\' AND castings.ord != 1')
+    .distinct  # both. group(:id) or .distinct work
 
 end
 
