a5ec00e26405956b8eace8c65e8ec7eaacc3e90b
jrupinski <rupinskijakub@gmail.com>
2021-01-19 23:03:29 +0100

    Implement half of queries in 02_queries.rb
    Why:
    - Implement:
        - #eighties_b_movies
        - #bad_years
        - #cast_list(title)

--- sql/active_record_2/movie_buff/movie_buff/02_queries.rb
+++ sql/active_record_2/movie_buff/movie_buff/02_queries.rb
@@ -2,15 +2,26 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where(yr: 1980..1989, score: 3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie
+  .group(:yr)
+  .having('MAX(movies.score) < 8')
+  .pluck(:yr)
 end
 
 def cast_list(title)
+  Actor
+    .joins(movies: :castings)
+    .select(:id, :name)
+    .group('actors.id, castings.ord')
+    .where(movies: { title: title })
+    .order('castings.ord')
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
 
