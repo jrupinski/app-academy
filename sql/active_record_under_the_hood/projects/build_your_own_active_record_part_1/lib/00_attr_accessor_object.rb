2dbdfa5efd25d917e864429d68dccbd2cf3ac484
jrupinski <rupinskijakub@gmail.com>
2021-03-01 23:00:06 +0100

    Write custom #attr_accessor, based on test suite

--- sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/00_attr_accessor_object.rb
+++ sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/00_attr_accessor_object.rb
@@ -1,5 +1,13 @@
 class AttrAccessorObject
+  # Implement ::attr_accessor
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      # custom getter
+      define_method(name) { self.instance_variable_get("@#{name}")}
+      # custom setter
+      define_method("#{name}=") do |value|
+        self.instance_variable_set("@#{name}", value)
+      end
+    end
   end
 end
