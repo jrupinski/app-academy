3908fb9e61dd3586bd695f02a4a4a55f8a75f381
jrupinski <rupinskijakub@gmail.com>
2021-03-27 22:23:12 +0100

    Implement Searchable module, mixin it to SQLObject

--- sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/02_searchable.rb
+++ sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/02_searchable.rb
@@ -2,11 +2,35 @@ require_relative 'db_connection'
 require_relative '01_sql_object'
 
 module Searchable
+  #
+  # Custom made ActiveDirectory-like ::where to search for rows by given criteria 
+  #
+  # @param [Hash] params attributes and values to search for in model's table
+  #
+  # @return [self] Return Child's Class type objects
+  #
   def where(params)
-    # ...
+    columns = params.keys.map(&:to_s)
+    values = params.values
+
+    where_line = columns
+      .map { |col| "#{col} = ?" }
+      .join(' AND ')
+
+    results = DBConnection.execute(<<-SQL, *values)
+      SELECT
+        *
+      FROM
+        #{table_name}
+      WHERE
+        #{where_line}
+    SQL
+
+    parse_all(results)
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end
