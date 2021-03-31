8b65624bf81a13367a11474e0a0e33378ede62de
jrupinski <rupinskijakub@gmail.com>
2021-04-01 00:06:49 +0200

    Add BelongsToOptions and HasManyOptions classes
    Why:
    - Add BelongsToOptions and HasManyOptions to mixin into has_many
      and belongs_to custom-made modules

--- sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/03_associatable.rb
+++ sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/03_associatable.rb
@@ -9,24 +9,44 @@ class AssocOptions
     :primary_key
   )
 
+  #
+  # Fetch object's class/model from class's class_name string
+  #
+  # @return [Object] Object of class_name 
+  #
   def model_class
-    # ...
+    self.class_name.constantize
   end
 
+  #
+  # Returns current row/object's class/model table name
+  #
+  # @return [String] table name of current object/child
+  #
   def table_name
-    # ...
+    self.model_class.table_name
   end
 end
 
+#
+# Custom made class that holds values for belongs_to association (just like in rails's active directory)
+#
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    @primary_key = options[:primary_key] || :id
+    @foreign_key = options[:foreign_key] || "#{name}_id".downcase.to_sym
+    @class_name = options[:class_name] || name.camelcase
   end
 end
 
+#
+# # Custom made class that holds values for has_many association (just like in rails's active directory)
+#
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    @primary_key = options[:primary_key] || :id
+    @foreign_key = options[:foreign_key] || "#{self_class_name}_id".downcase.to_sym
+    @class_name = options[:class_name] || name.singularize.camelcase
   end
 end
 
