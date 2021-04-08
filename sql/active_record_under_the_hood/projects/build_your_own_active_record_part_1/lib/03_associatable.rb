2d60b49bbb64b28b9598d9119d2ce81fccdef36f
jrupinski <rupinskijakub@gmail.com>
2021-04-09 00:04:14 +0200

    Implement custom ActiveRecord-like #belongs_to

--- sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/03_associatable.rb
+++ sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/03_associatable.rb
@@ -15,7 +15,7 @@ class AssocOptions
   # @return [Object] Object of class_name 
   #
   def model_class
-    self.class_name.constantize
+    class_name.constantize
   end
 
   #
@@ -24,7 +24,7 @@ class AssocOptions
   # @return [String] table name of current object/child
   #
   def table_name
-    self.model_class.table_name
+    model_class.table_name
   end
 end
 
@@ -34,26 +34,47 @@ end
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     @primary_key = options[:primary_key] || :id
-    @foreign_key = options[:foreign_key] || "#{name}_id".downcase.to_sym
-    @class_name = options[:class_name] || name.camelcase
+    @foreign_key = options[:foreign_key] || "#{name.to_s}_id".to_sym
+    @class_name = options[:class_name] || name.to_s.camelcase
   end
 end
 
 #
 # # Custom made class that holds values for has_many association (just like in rails's active directory)
+# Use String#underscore so CamelCase classes get correctly named as keys
 #
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     @primary_key = options[:primary_key] || :id
-    @foreign_key = options[:foreign_key] || "#{self_class_name}_id".downcase.to_sym
-    @class_name = options[:class_name] || name.singularize.camelcase
+    @foreign_key = options[:foreign_key] || "#{self_class_name.underscore}_id".to_sym
+    @class_name = options[:class_name] || name.to_s.singularize.camelcase
   end
 end
 
 module Associatable
-  # Phase IIIb
+  #
+  # custom-made ActiveRecord-like belongs_to association.
+  # Uses custom made methods too, that's why #where is using a hash 
+  #
+  # @param [String] name Name of class association
+  # @param [Hash] options Optional hash with non-default key and attribute values
+  #
+  # @return [Class] Returns single object of association's class
+  #
   def belongs_to(name, options = {})
-    # ...
+  options = BelongsToOptions.new(name, options)
+  define_method(name) do
+      fg_key = options.send(:foreign_key)
+      fg_key_value = self.send(fg_key)
+      model = options.model_class
+      # options
+      #   .model_class
+      #   .where(options.primary_key => fg_key_value)
+      #   .first
+      
+      # refactored method:
+      model.find(fg_key_value)
+    end
   end
 
   def has_many(name, options = {})
@@ -67,4 +88,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
