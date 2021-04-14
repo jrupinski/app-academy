c3d13ed970be85a483262353affe407eb1ec2137
jrupinski <rupinskijakub@gmail.com>
2021-04-14 21:50:42 +0200

    Refactor, add doc, create custom #has_one_through

--- sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/03_associatable.rb
+++ sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/03_associatable.rb
@@ -62,26 +62,31 @@ module Associatable
   # @return [Class] Returns single object of association's class
   #
   def belongs_to(name, options = {})
-  options = BelongsToOptions.new(name, options)
-  define_method(name) do
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      options = self.class.assoc_options[name]
       fg_key = options.send(:foreign_key)
       fg_key_value = self.send(fg_key)
       model = options.model_class
-      # options
-      #   .model_class
-      #   .where(options.primary_key => fg_key_value)
-      #   .first
-      
-      # refactored method:
       model.find(fg_key_value)
     end
   end
 
+  #
+  # custom-made ActiveRecord-like has_many association.
+  # Uses custom made methods too, that's why #where is using a hash 
+  #
+  # @param [String] name Name of class association
+  # @param [Hash] options Optional hash with non-default key and attribute values
+  #
+  # @return [Array] Returns a list of objects of association's class
+  #
   def has_many(name, options = {})
-    self_class_name = self.to_s
-    options = HasManyOptions.new(name, self_class_name, options)
+    self.assoc_options[name] = HasManyOptions.new(name, self.name, options)
 
-    define_method(name) do 
+    define_method(name) do
+      options = self.class.assoc_options[name]
       primary_key = options.primary_key
       primary_key_value = self.send(primary_key)
       fg_key = options.foreign_key
@@ -92,8 +97,17 @@ module Associatable
       end
     end
 
+  #
+  # Store association options inside a class variable (belongs_to, has_many).
+  # For easier access when creating methods that use these associations.
+  # The hash is lazily initialized
+  #
+  # @return [Hash] Hash with association names as keys, and their options (class objects) as values
+  #
   def assoc_options
     # Wait to implement this in Phase IVa. Modify `belongs_to`, too.
+    @assoc_options ||= {}
+    @assoc_options
   end
 end
 
--- sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/04_associatable2.rb
+++ sql/active_record_under_the_hood/projects/build_your_own_active_record_part_1/lib/04_associatable2.rb
@@ -4,7 +4,47 @@ require_relative '03_associatable'
 module Associatable
   # Remember to go back to 04_associatable to write ::assoc_options
 
+  #
+  # ActiveRecord-like has_one_through relation method for table records.
+  # A belongs-to that goes through an existing belongs-to relation.
+  #
+  # @param [String] name name of object name/method for relation in object the current object belongs to 
+  # @param [String] through_name name of Model current object belongs to, to travel through
+  # @param [String] source_name name of Model which current object's model parent belongs to
+  #
+  # @return [Class] Returns single model object current object has relation with
+  #
   def has_one_through(name, through_name, source_name)
-    # ...
+    # the source and through options fetching is moved inside define_method
+    # Class it belongs to, or Class it goes throgh might not exist when assigning it,
+    # causing a crash. This way the class it belongs to has to be called first (so it has to exists first!)
+    define_method(name) do
+      through_options = self.class.assoc_options[through_name]
+      source_options = through_options.model_class.assoc_options[source_name]
+
+      through_table = through_options.table_name
+      through_fk = through_options.foreign_key
+      through_pk = through_options.primary_key
+
+      source_table = source_options.table_name
+      source_pk = source_options.primary_key
+      source_fk = source_options.foreign_key
+
+      through_fk_val = self.send(through_fk)
+      results = DBConnection.execute(<<-SQL, through_fk_val)
+        SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN 
+          #{source_table} 
+        ON 
+          #{through_table}.#{source_fk} = #{source_table}.#{source_pk}
+        WHERE
+          #{through_table}.#{through_pk} = ?
+      SQL
+
+      source_options.model_class.parse_all(results).first
+    end
   end
 end
