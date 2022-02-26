44402aa01774b48156921d69c24389ebd632daf6
jrupinski <rupinskijakub@gmail.com>
2022-02-26 23:53:16 +0100

    Add seeds, #index to artworks_controller

--- rails/art_share_api/db/seeds.rb
+++ rails/art_share_api/db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_1 = User.create!(username: 'tester')
+user_2 = User.create!(username: 'tester 2')
+user_3 = User.create!(username: 'tester 3')
+
+artwork_1 = Artwork.create!(
+  title: "ass",
+  image_url: "www.reddit.com",
+  artist_id: user_1.id,
+)
+
+artwork_2 = Artwork.create!(
+  title: "pepe",
+  image_url: "www.wykop.pl",
+  artist_id: user_2.id,
+)
+
+_artwork_share_1 = ArtworkShare.create!(
+  artwork_id: artwork_1.id,
+  viewer_id: user_2.id
+)
+
+_artwork_share_2 = ArtworkShare.create!(
+  artwork_id: artwork_2.id,
+  viewer_id: user_3.id
+)
