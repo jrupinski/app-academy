9b361514bcdae13c059f83e6c49de97d6bb4434d
jrupinski <rupinskijakub@gmail.com>
2022-04-05 22:03:05 +0200

    Add CommentsController and its actions

--- rails/art_share_api_part_2/config/routes.rb
+++ rails/art_share_api_part_2/config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[create destroy index show update]
   resources :artwork_shares, only: %i[create destroy]
-  resources :comments, only: %i[create destroy index show]
+  resources :comments, only: %i[create destroy index]
 end
