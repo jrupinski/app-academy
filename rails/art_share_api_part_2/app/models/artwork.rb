205c523f6a18aa2ac9902c0dd98a76fe3fe9c358
jrupinski <rupinskijakub@gmail.com>
2022-04-06 22:19:25 +0200

    Add #liked_users relation for Artwork and Comment

--- rails/art_share_api_part_2/app/models/artwork.rb
+++ rails/art_share_api_part_2/app/models/artwork.rb
@@ -8,4 +8,7 @@ class Artwork < ApplicationRecord
   has_many :shared_viewers, through: :artwork_shares, source: :viewer
   has_many :comments, dependent: :destroy
   has_many :likes, as: :likeable
+  has_many :liked_users,
+           through: :likes,
+           source: :user
 end
--- rails/art_share_api_part_2/app/models/comment.rb
+++ rails/art_share_api_part_2/app/models/comment.rb
@@ -4,4 +4,7 @@ class Comment < ApplicationRecord
   belongs_to :author, foreign_key: 'user_id', class_name: 'User'
 
   has_many :likes, as: :likeable
+  has_many :liked_users,
+          through: :likes,
+          source: :user
 end
