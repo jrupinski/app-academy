968e9639a56fa53921564ce45e1c2ddbd3f958b7
jrupinski <rupinskijakub@gmail.com>
2022-04-13 18:48:43 +0200

    Update existing migrations to be reversible

--- rails/auth_1_creating_users/db/migrate/20140427232923_remove_passwords_from_users.rb
+++ rails/auth_1_creating_users/db/migrate/20140427232923_remove_passwords_from_users.rb
@@ -1,7 +1,6 @@
 class RemovePasswordsFromUsers < ActiveRecord::Migration[7.0]
   def change
-    remove_column :users, :password
-    add_column :users, :password_digest, :string
-    change_column :users, :password_digest, :string, null: false
+    remove_column :users, :password, :string
+    add_column :users, :password_digest, :string, null: false
   end
 end
