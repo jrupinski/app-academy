7266ba3902337b44aed1291ca79d5316fb7f1e30
jrupinski <rupinskijakub@gmail.com>
2022-04-20 20:39:15 +0200

    Add #find_by_credentials, add documentation

--- rails/rails_auth_project/app/models/user.rb
+++ rails/rails_auth_project/app/models/user.rb
@@ -8,15 +8,58 @@ class User < ApplicationRecord
 
   attr_reader :password
 
+  #
+  # Hash password string and assign it to User
+  #
+  # @param [String] password User password
+  #
+  # @return [String] Hashed password
+  #
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  #
+  # Check if password String matches the password digest when hashed
+  #
+  # @param [String] password Password string 
+  #
+  # @return [Boolean] True if password matches
+  #
+  def is_password?(password)
+    hashed_password = BCrypt::Password.new(self.password_digest)
+    hashed_password.is_password?(password)
+  end
+
+  #
+  # Find User by his username and password
+  #
+  # @param [String] username User's username
+  # @param [String] password User's password
+  #
+  # @return [User] User matching the username and password
+  #
+  def self.find_by_credentials(username:, password:)
+    user = User.find_by(username: username)
+
+    return user if user && user.is_password?(password)
+  end
+
+  #
+  # Generate a 16-byte session token to assign to user
+  #
+  # @return [String] Session token
+  #
   def self.generate_session_token
     SecureRandom.urlsafe_base64(16) # default is already 16 bytes, but documentation says it might change
   end
 
+  #
+  # Removes old session token and generates a new one, assigning it to User
+  #
+  # @return [String] Session Token
+  #
   def reset_session_token!
     session_token = self.class.generate_session_token
     save!
@@ -25,11 +68,12 @@ class User < ApplicationRecord
 
   private
 
+  #
+  # Returns existing User's session token, or assings a new one if it doesn't exist
+  #
+  # @return [String] Session Token
+  #
   def ensure_session_token
-    # copy-pasted reason for '||=' operator:
-    # we must be sure to use the ||= operator instead of = or ||, otherwise
-    # we will end up with a new session token every time we create
-    # a new instance of the User class. This includes finding it in the DB!
     self.session_token ||= self.class.generate_session_token
   end
 end
