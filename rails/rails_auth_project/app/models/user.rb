6d9c2b4bde56c2c7071fe0d6b0cd5e2e6b661db4
jrupinski <rupinskijakub@gmail.com>
2022-04-20 14:07:46 +0200

    Add bcrypt gem, add password validations to User

--- rails/rails_auth_project/app/models/user.rb
+++ rails/rails_auth_project/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
