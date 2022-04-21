756cd5c34ed57c897e52f0cbe76ee6b6ef2cc521
jrupinski <rupinskijakub@gmail.com>
2022-04-21 16:51:46 +0200

    Add Session controller and it's routes

---
+++ rails/99_cats_II_auth/app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by_credentials(username: user_params[:username], password: user_params[:password])
+
+    if @user.nil?
+      flash[:errors] = ['Wrong username or password!']
+      redirect_to new_session_url
+    else
+      session[:session_token] = @user.reset_session_token!
+      redirect_to cats_url(@user)
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
