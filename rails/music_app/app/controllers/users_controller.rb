c25762d733522a35bb1c48fb346358e63be471cb
jrupinski <rupinskijakub@gmail.com>
2022-04-27 18:13:31 +0200

    Minor routes touchup, add default root route

--- rails/music_app/app/controllers/users_controller.rb
+++ rails/music_app/app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
 
     if user.save
       login_user!(user)
-      redirect_to users_path
+      redirect_to user_path(user.id)
     else
       flash.now[:errors] = user.errors.full_messages
       render :new
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   def delete
     user = User.find(params[:id])
     user&.destroy
-    redirect_to users_path
+    redirect_to new_session_path
   end
 
   private
--- rails/music_app/config/routes.rb
+++ rails/music_app/config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'sessions#new'
 
   resources :users
-  resource :session
+  resource :session, only: %i[new create destroy]
 end
