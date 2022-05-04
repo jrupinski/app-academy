22d540c3ddb04f95bdf877326437596f252db641
jrupinski <rupinskijakub@gmail.com>
2022-05-04 18:20:13 +0200

    Setup Shoulda-matchers, email domain for testing

--- rails/music_app_testing_with_capybara/config/environments/test.rb
+++ rails/music_app_testing_with_capybara/config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Configure default mail server
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
 end
