c64c43431ad402445b47d569c9182b7d18efaad0
jrupinski <rupinskijakub@gmail.com>
2022-05-17 11:26:42 +0200

    Add more integration tests for goals CRUD

--- rails/personal-goals-app-project/spec/features/goal_completeness_spec.rb
+++ rails/personal-goals-app-project/spec/features/goal_completeness_spec.rb
@@ -53,5 +53,49 @@ feature 'tracking goal progress/completeness' do
         expect(page).to have_content goal.title
       end
     end
+
+    context 'in Goals#index page' do
+      it 'Allows user to toggle goal status to complete' do
+        visit goals_path
+        click_button "goal_#{goal.id}_completed"
+        expect(page).to have_content 'Completed'
+      end
+
+      it 'Does not allow other users to edit goal status' do
+        visit goals_path
+        expect(page).to have_button "goal_#{goal.id}_completed"
+        click_button 'Log out'
+        expect(page).to_not have_button "goal_#{goal.id}_completed"
+      end
+
+      it 'Does not redirect to a different page' do
+        visit goals_path
+        click_button "goal_#{goal.id}_completed"
+        expect(page).to have_content 'Goal completedness'
+        expect(page).to have_content goal.title
+      end
+    end
+
+    context 'in User profile page' do
+      it 'Allows user to toggle goal status to complete' do
+        visit user_path(user)
+        click_button "goal_#{goal.id}_completed"
+        expect(page).to have_content 'Completed'
+      end
+
+      it 'Does not allow other users to edit goal status' do
+        visit user_path(user)
+        expect(page).to have_button "goal_#{goal.id}_completed"
+        click_button 'Log out'
+        expect(page).to_not have_button "goal_#{goal.id}_completed"
+      end
+
+      it 'Does not redirect to a different page' do
+        visit user_path(user)
+        click_button "goal_#{goal.id}_completed"
+        expect(page).to have_content 'Goal completedness'
+        expect(page).to have_content goal.title
+      end
+    end
   end
 end
