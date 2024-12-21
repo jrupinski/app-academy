484cfa7c750a031346fb160a919b6d1bf89df5db
jrupinski <rupinskijakub@gmail.com>
2024-12-21 19:35:04 +0100

    feat: Add phase 2 for js intro problems 2

--- javascript/javascript_fundamentals/intro_javascript_problems_part_2/intro_javascript_problems_part_2.js
+++ javascript/javascript_fundamentals/intro_javascript_problems_part_2/intro_javascript_problems_part_2.js
@@ -12,3 +12,25 @@ function printCallback(arr) {
 		console.log(el);
 	})
 }
+
+// Phase II: Constructors, Prototypes, and this
+function Elephant(name, height, array_of_tricks) {
+	this.name = name;
+	this.heigth = height;
+	this.array_of_tricks = array_of_tricks;
+}
+
+Elephant.prototype.trumpet = function() {
+	console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+}
+Elephant.prototype.grow = function() {
+	this.heigth += 12;
+}
+Elephant.prototype.addTrick = function(trick) {
+	this.array_of_tricks << trick;
+}
+Elephant.prototype.play = function() {
+	random_trick = Math.floor(Math.random() * this.array_of_tricks.length);
+	return this.array_of_tricks[random_trick];
+}
+
