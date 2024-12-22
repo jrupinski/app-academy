75c4498ccf917994e4ef015c3144f35740b6beb6
jrupinski <rupinskijakub@gmail.com>
2024-12-22 12:59:58 +0100

    refactor: Refactor existing code to more modern standards

--- javascript/javascript_fundamentals/intro_javascript_problems_part_2/intro_javascript_problems_part_2.js
+++ javascript/javascript_fundamentals/intro_javascript_problems_part_2/intro_javascript_problems_part_2.js
@@ -1,19 +1,17 @@
 // Phase I: Callbacks
 function titleize(arr, callback_fn) {
-	var mapped_arr = arr.map(function(el) {
-		return `Mx. ${el} Jingleheimer Schmidt`;
-	});
+	var mapped_arr = arr.map(el => `Mx. ${el} Jingleheimer Schmidt`);
 
 	callback_fn(mapped_arr);
-}
+};
 
-function printCallback(arr) {
-	arr.forEach(function(el) {
-		console.log(el);
-	})
-}
+
+titleize(["Mary", "Joe", "Mama"], (names) => {
+	names.forEach(name => console.log(name));
+});
 
 // Phase II: Constructors, Prototypes, and this
+// Old school way
 function Elephant(name, height, array_of_tricks) {
 	this.name = name;
 	this.heigth = height;
@@ -34,3 +32,25 @@ Elephant.prototype.play = function() {
 	return this.array_of_tricks[random_trick];
 }
 
+// New way
+class Elephant {
+	constructor(name, height, tricks) {
+		this.name = name;
+		this.height = height;
+		this.tricks = tricks;
+	}
+
+	trumpet() {
+		console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRR!!!!!!!'`);
+	}
+	grow() {
+		this.height += 12;
+	}
+	addTrick(trick) {
+		this.array_of_tricks << trick;
+	}
+	play() {
+		random_trick = Math.floor(Math.random() * this.array_of_tricks.length);
+		return this.array_of_tricks[random_trick];
+	}
+}
